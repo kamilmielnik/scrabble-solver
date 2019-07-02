e6ec9a0b3334640937c8480f312cd0c50a3d2bc3
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2019-07-02 23:34:20 +0700

    Extract validation functions to a separate file

--- packages/backend/src/endpoints/solve/index.js
+++ packages/backend/src/endpoints/solve/index.js
@@ -3,6 +3,7 @@ import * as configs from '@scrabble-solver/configs';
 import { Board, Config, Tile } from '@scrabble-solver/models';
 import Solver from '@scrabble-solver/solver';
 import Trie from '@scrabble-solver/trie';
+import { validateBoard, validateConfigId } from './validate';
 
 export default (locale, filepath) => {
   const collection = createCollection(filepath);
@@ -37,74 +38,4 @@ const parseRequest = (request, locale) => {
     config,
     tiles: (tiles || []).map(Tile.fromJson)
   };
-};
-
-const validateConfigId = (configId) => {
-  if (!Object.keys(configs).includes(configId)) {
-    throw new Error(`Invalid "configId" parameter: not one of ${Object.keys(configs).join('/')}`);
-  }
-};
-
-const validateBoard = (board, config) => {
-  if (!Array.isArray(board)) {
-    throw new Error('Invalid "board" parameter: not an array');
-  }
-
-  if (board.length !== config.boardHeight) {
-    throw new Error(`Invalid "board" parameter: does not have ${config.boardHeight} rows`);
-  }
-
-  try {
-    board.forEach((row, rowIndex) => validateRow(row, rowIndex, config));
-  } catch (error) {
-    throw new Error(`Invalid "board" parameter: ${error.message}`);
-  }
-};
-
-const validateRow = (row, rowIndex, config) => {
-  if (row.length !== config.boardWidth) {
-    throw new Error(`board[${rowIndex}] does not have ${config.boardWidth} cells`);
-  }
-
-  row.forEach((cell, cellIndex) => validateCell(cell, rowIndex, cellIndex, config));
-};
-
-const validateCell = (cell, rowIndex, cellIndex, config) => {
-  if (typeof cell !== 'object') {
-    throw new Error(`board[${rowIndex}][${cellIndex}] is not an object`);
-  }
-
-  const { x, y, tile, isEmpty } = cell;
-
-  if (x < 0 || x >= config.boardWidth) {
-    throw new Error(`board[${rowIndex}][${cellIndex}].x is out of bounds`);
-  }
-
-  if (y < 0 || y >= config.boardHeight) {
-    throw new Error(`board[${rowIndex}][${cellIndex}].y is out of bounds`);
-  }
-
-  if (typeof isEmpty !== 'boolean') {
-    throw new Error(`board[${rowIndex}][${cellIndex}].isEmpty is not a boolean`);
-  }
-
-  validateTile(tile, rowIndex, cellIndex, config);
-};
-
-const validateTile = (tile, rowIndex, cellIndex, config) => {
-  if (typeof tile !== 'object') {
-    throw new Error(`board[${rowIndex}][${cellIndex}].tile is not an object`);
-  }
-
-  if (tile !== null) {
-    const { character, isBlank } = tile;
-
-    if (!config.alphabet.includes(character)) {
-      throw new Error(`board[${rowIndex}][${cellIndex}].tile.character is not valid`);
-    }
-
-    if (typeof isBlank !== 'boolean') {
-      throw new Error(`board[${rowIndex}][${cellIndex}].tile.isBlank is not a boolean`);
-    }
-  }
 };
