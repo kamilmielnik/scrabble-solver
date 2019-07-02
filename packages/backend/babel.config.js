00913f3bff4fb1d488e25dbe0d5a043f87691a1b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2019-07-03 02:09:27 +0700

    Refactor search-en-dictionary

--- packages/backend/babel.config.js
+++ packages/backend/babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ],
   plugins: ['@babel/plugin-proposal-class-properties']
 };
--- packages/backend/src/endpoints/search-en-dictionary.js
+++ packages/backend/src/endpoints/search-en-dictionary.js
@@ -1,35 +1,34 @@
 import http from 'http';
+import { URLSearchParams } from 'url';
 import { WordDefinition, NullWordDefinition } from '@scrabble-solver/models';
 
 const API_KEY = 'd0c21cb3cbc3415984a2a0486da075e54aa68091c33a680d9';
-const MAX_RESULTS = 10;
-const SOURCE_DICTIONARIES = 'all';
-
-export default (request, response) =>
-  getJson(getOptions(request.params.word))
-    .then((results) => {
-      const wordDefinition = new WordDefinition({
-        definitions: results.map(({ text }) => text),
-        isAllowed: results.length > 0,
-        word: request.params.word
-      });
-      response.send(wordDefinition.toJson());
-    })
-    .catch(() => response.send(NullWordDefinition));
-
-const getOptions = (word) => ({
-  hostname: 'api.wordnik.com',
-  path: getWordnikUrl(word),
-  headers: {
-    'Content-Type': 'application/json; charset=utf-8'
-  }
+const SEARCH_PARAMS = new URLSearchParams({
+  api_key: API_KEY, // eslint-disable-line camelcase
+  limit: 10,
+  sourceDictionaries: 'all'
 });
 
-const getWordnikUrl = (word) =>
-  [
-    `/v4/word.json/${word}/definitions?`,
-    [`api_key=${API_KEY}`, `limit=${MAX_RESULTS}`, `sourceDictionaries=${SOURCE_DICTIONARIES}`].join('&')
-  ].join('');
+export default async (request, response) => {
+  try {
+    const { word } = request.params;
+    const results = await getJson({
+      hostname: 'api.wordnik.com',
+      path: `/v4/word.json/${word}/definitions?${SEARCH_PARAMS}`,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    });
+    const wordDefinition = new WordDefinition({
+      definitions: results.map(({ text }) => text).filter(Boolean),
+      isAllowed: results.length > 0,
+      word
+    });
+    response.send(wordDefinition.toJson());
+  } catch (error) {
+    response.send(NullWordDefinition);
+  }
+};
 
 const getJson = (options) =>
   new Promise((resolve, reject) =>
@@ -41,7 +40,11 @@ const getJson = (options) =>
           data += chunk;
         });
         response.on('end', () => {
-          resolve(JSON.parse(data));
+          try {
+            resolve(JSON.parse(data));
+          } catch (error) {
+            reject(error);
+          }
         });
       })
       .on('error', reject)
