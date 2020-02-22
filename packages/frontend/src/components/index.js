e19d4fe65c8557594f9e6dfc4eba65220df3739b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-02-22 20:00:52 +0700

    Refactor Tile component export

--- packages/frontend/src/components/index.js
+++ packages/frontend/src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Button } from './Button';
 export { default as Loading } from './Loading';
 export { default as Setting } from './Setting';
-export { default as Tile } from './tile';
+export { default as Tile } from './Tile';
