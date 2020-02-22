ef1089119dfadac6882ee3531a5b073ec93dacf8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-02-22 21:37:03 +0700

    Delete cross & sort icons

--- packages/frontend/src/components/icons/index.js
+++ packages/frontend/src/components/icons/index.js
@@ -1,6 +1,4 @@
-export { default as Cross } from './cross';
 export { default as FlagGb } from './flag-gb';
 export { default as FlagPl } from './flag-pl';
 export { default as FlagUs } from './flag-us';
 export { default as QuestionMark } from './question-mark';
-export { default as Sort } from './sort';
