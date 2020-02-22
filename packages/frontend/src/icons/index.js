4109d270048924d34e34ec923497e3ff30be92a6
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-02-22 21:38:42 +0700

    Rename icons files

--- packages/frontend/src/icons/index.js
+++ packages/frontend/src/icons/index.js
@@ -1,4 +1,4 @@
-export { default as FlagGb } from './flag-gb';
-export { default as FlagPl } from './flag-pl';
-export { default as FlagUs } from './flag-us';
-export { default as QuestionMark } from './question-mark';
+export { default as FlagGb } from './FlagGb';
+export { default as FlagPl } from './FlagPl';
+export { default as FlagUs } from './FlagUs';
+export { default as QuestionMark } from './QuestionMark';
