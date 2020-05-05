ef0da4fac83d3f9d52f4bbe79b9644025cee29c2
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-05-05 22:04:34 +0700

    1.4.1

---
+++ dist/scrabble-solver-frontend/precache-manifest.042f4468d9422e35fdfd75feaa2fab3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce072c5bfe2445b55dd3d4ae47f4ff96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7788928a235ee14e5210",
+    "url": "/static/css/2.7ef46b21.chunk.css"
+  },
+  {
+    "revision": "19d2e6cee823b6ca113d",
+    "url": "/static/css/main.2ecbabc0.chunk.css"
+  },
+  {
+    "revision": "7788928a235ee14e5210",
+    "url": "/static/js/2.7c865eac.chunk.js"
+  },
+  {
+    "revision": "19d2e6cee823b6ca113d",
+    "url": "/static/js/main.70f0d892.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "851a12f98b6fb5f2edd24b7ab518096e",
+    "url": "/static/media/bg.851a12f9.png"
+  }
+]);
