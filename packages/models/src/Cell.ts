03c372b14a2885c54946ce19594e3b874065952a
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-11 03:38:02 +0700

    Implement Cell & Tile in TypeScript

---
+++ packages/models/src/Cell.ts
@@ -0,0 +1,79 @@
+import Tile, { TileJson } from './Tile';
+
+export interface CellJson {
+  isEmpty: boolean;
+  tile: TileJson;
+  x: number;
+  y: number;
+}
+
+class Cell {
+  static fromJson(json: CellJson) {
+    if (!json) {
+      return Cell.Null;
+    }
+
+    return new Cell({
+      isEmpty: json.isEmpty,
+      tile: Tile.fromJson(json.tile),
+      x: json.x,
+      y: json.y
+    });
+  }
+
+  public static Null: Cell = Object.freeze(
+    new Cell({
+      isEmpty: true,
+      tile: Tile.Null,
+      x: 0,
+      y: 0
+    })
+  );
+
+  public readonly isEmpty: boolean;
+
+  public readonly tile: Tile;
+
+  public readonly x: number;
+
+  public readonly y: number;
+
+  constructor({ isEmpty = true, tile = Tile.Null, x, y }: { isEmpty: boolean; tile: Tile; x: number; y: number }) {
+    this.isEmpty = isEmpty;
+    this.tile = tile;
+    this.x = x;
+    this.y = y;
+  }
+
+  public clone(): Cell {
+    return new Cell({
+      isEmpty: this.isEmpty,
+      tile: this.tile.clone(),
+      x: this.x,
+      y: this.y
+    });
+  }
+
+  public hasTile(): boolean {
+    return this.tile !== Tile.Null;
+  }
+
+  public isCandidate(): boolean {
+    return this.isEmpty && this.hasTile();
+  }
+
+  public toJson(): CellJson {
+    return {
+      isEmpty: this.isEmpty,
+      tile: this.tile.toJson(),
+      x: this.x,
+      y: this.y
+    };
+  }
+
+  public toString(): string {
+    return String(this.tile);
+  }
+}
+
+export default Cell;
---
+++ packages/models/src/Tile.ts
@@ -0,0 +1,50 @@
+import { EMPTY_CELL } from '@scrabble-solver/constants';
+
+export interface TileJson {
+  character: string;
+  isBlank: boolean;
+}
+
+class Tile {
+  public static fromJson(json: TileJson): Tile {
+    if (!json) {
+      return Tile.Null;
+    }
+
+    return new Tile({
+      character: json.character,
+      isBlank: json.isBlank
+    });
+  }
+
+  public static Null: Tile = Object.freeze(new Tile({ character: '', isBlank: false }));
+
+  public readonly character: string;
+
+  public readonly isBlank: boolean;
+
+  constructor({ character, isBlank = false }: { character: string; isBlank: boolean }) {
+    this.character = character;
+    this.isBlank = isBlank;
+  }
+
+  public clone(): Tile {
+    return new Tile({
+      character: this.character,
+      isBlank: this.isBlank
+    });
+  }
+
+  public toJson(): TileJson {
+    return {
+      character: this.character,
+      isBlank: this.isBlank
+    };
+  }
+
+  public toString(): string {
+    return this.character;
+  }
+}
+
+export default Tile;
