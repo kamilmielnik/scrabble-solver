66efa77a1c6f2fd914cbee48757e0588d79b4cdb
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-11 03:58:10 +0700

    Introduce index.ts

---
+++ packages/models/src/index.ts
@@ -0,0 +1,9 @@
+export { default as Board } from './Board';
+export { default as Cell } from './Cell';
+// export { default as Config } from './Config';
+// export { default as HorizontalPattern } from './horizontal-pattern';
+// export { default as Pattern } from './pattern';
+// export { default as Result } from './result';
+export { default as Tile } from './Tile';
+// export { default as VerticalPattern } from './vertical-pattern';
+// export { default as WordDefinition, NullWordDefinition } from './word-definition';
