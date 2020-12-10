0f476248fbf8c5d85669947759bcd5d1c1bc6c78
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-11 04:25:59 +0700

    Implement WordDefinition in TypeScript

---
+++ packages/models/src/WordDefinition.ts
@@ -0,0 +1,37 @@
+interface WordDefinitionJson {
+  definitions: string[];
+  isAllowed: boolean;
+  word: string;
+}
+
+class WordDefinition {
+  public static Null = Object.freeze(
+    new WordDefinition({
+      definitions: [],
+      isAllowed: false,
+      word: ''
+    })
+  );
+
+  public readonly definitions: string[];
+
+  public readonly isAllowed: boolean;
+
+  public readonly word: string;
+
+  constructor({ definitions, isAllowed, word }: { definitions: string[]; isAllowed: boolean; word: string }) {
+    this.definitions = definitions;
+    this.isAllowed = isAllowed;
+    this.word = word;
+  }
+
+  toJson(): WordDefinitionJson {
+    return {
+      definitions: this.definitions,
+      isAllowed: this.isAllowed,
+      word: this.word
+    };
+  }
+}
+
+export default WordDefinition;
