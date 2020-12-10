de54388cf9b8e6bc977bd28a4a6c27eea473f2c4
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-11 04:22:11 +0700

    Export bonuses

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,5 +1,8 @@
 export { default as Board } from './Board';
+export { default as Bonus } from './Bonus';
+export { default as BonusValue } from './BonusValue';
 export { default as Cell } from './Cell';
+export { default as CharacterBonus } from './CharacterBonus';
 // export { default as Config } from './Config';
 // export { default as HorizontalPattern } from './horizontal-pattern';
 // export { default as Pattern } from './pattern';
@@ -7,3 +10,4 @@ export { default as Cell } from './Cell';
 export { default as Tile } from './Tile';
 // export { default as VerticalPattern } from './vertical-pattern';
 // export { default as WordDefinition, NullWordDefinition } from './word-definition';
+export { default as WordBonus } from './WordBonus';
