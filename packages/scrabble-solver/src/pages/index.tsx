8d771bcfe93e70a96312050c8d11ce83d5ba51c0
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-20 03:09:51 +0700

    Add box shadows

--- packages/scrabble-solver/src/pages/index.tsx
+++ packages/scrabble-solver/src/pages/index.tsx
@@ -5,7 +5,11 @@ import styles from './index.module.scss';
 const Index = () => {
   return (
     <div className={styles.index}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <div className={styles.boardContainer}>board</div>
+
+        <div className={styles.tilesContainer}>tiles</div>
+      </div>
 
       <div className={styles.sidebar}></div>
     </div>
