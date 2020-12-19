4ed18e184675fca130d53e887bc19f8e5c084ccb
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-20 03:17:40 +0700

    Introduce layout

--- packages/scrabble-solver/src/pages/index.tsx
+++ packages/scrabble-solver/src/pages/index.tsx
@@ -5,13 +5,14 @@ import styles from './index.module.scss';
 const Index = () => {
   return (
     <div className={styles.index}>
+      <div className={styles.nav}>nav</div>
+
       <div className={styles.content}>
         <div className={styles.boardContainer}>board</div>
-
-        <div className={styles.tilesContainer}>tiles</div>
+        <div className={styles.sidebar}>sidebar</div>
       </div>
 
-      <div className={styles.sidebar}></div>
+      <div className={styles.tilesContainer}>tiles</div>
     </div>
   );
 };
