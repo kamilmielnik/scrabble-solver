853278ff89bb447fd42c4d0567613676359d5cf3
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-20 02:50:01 +0700

    Add page skeleton

--- packages/scrabble-solver/src/pages/index.tsx
+++ packages/scrabble-solver/src/pages/index.tsx
@@ -1,17 +1,13 @@
-import React, { useCallback, useEffect } from 'react';
-import Fingerprint2 from 'fingerprintjs2';
-
-import { SvgIcon } from 'components';
-import { flagGb, flagPl, flagUs } from 'icons';
+import React from 'react';
 
 import styles from './index.module.scss';
 
 const Index = () => {
   return (
-    <div className={styles.app}>
-      <SvgIcon icon={flagPl} />
-      <SvgIcon icon={flagGb} />
-      <SvgIcon icon={flagUs} />
+    <div className={styles.index}>
+      <div className={styles.content}></div>
+
+      <div className={styles.sidebar}></div>
     </div>
   );
 };
