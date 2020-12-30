fb5a2a3715767d72ffb70d46be21cb4d009172d5
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-12-31 00:06:07 +0700

    Hotfix scss imports

--- packages/scrabble-solver/src/@types/svg.d.ts
+++ packages/scrabble-solver/src/@types/svg.d.ts
@@ -11,3 +11,8 @@ declare module '*.svg' {
   const symbol: BrowserSpriteSymbol;
   export default symbol;
 }
+
+declare module '*.scss' {
+  const classesMap: Record<string, string>;
+  export default classesMap;
+}
