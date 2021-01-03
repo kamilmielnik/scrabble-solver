aa8b457dc8ae97a100b417693a894d41bc260854
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-03 15:54:05 +0700

    Optimize

--- packages/scrabble-solver/src/components/Loading/Loading.tsx
+++ packages/scrabble-solver/src/components/Loading/Loading.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 
 import { useTranslation } from 'state';
 
@@ -12,11 +12,12 @@ interface Props {
   children?: never;
   className?: string;
   estimatedDuration?: number;
+  floating?: boolean;
 }
 
-const Loading: FunctionComponent<Props> = ({ className, estimatedDuration }) => {
+const Loading: FunctionComponent<Props> = ({ className, estimatedDuration, floating }) => {
   const loading = useTranslation('loading');
-  const content: string[][] = [[loading.toUpperCase()]];
+  const content = useMemo<string[][]>(() => [[loading.toUpperCase()]], [loading]);
   const progress = useInfiniteProgress(estimatedDuration);
   const progressPercent = `${(progress * 100).toFixed(2)}%`;
 
@@ -24,10 +25,11 @@ const Loading: FunctionComponent<Props> = ({ className, estimatedDuration }) => 
     <div className={classNames(styles.loading, className)}>
       <div className={styles.dim} />
       <div className={styles.logo}>
-        <PlainTiles className={classNames(styles.logoGrayscale)} content={content} />
+        <PlainTiles className={classNames(styles.logoGrayscale)} content={content} floating={floating} />
         <PlainTiles
           className={classNames(styles.logoColor)}
           content={content}
+          floating={floating}
           style={{ clipPath: `polygon(0% 0%, ${progressPercent} 0, ${progressPercent} 100%, 0% 100%)` }}
         />
       </div>
--- packages/scrabble-solver/src/components/PlainTiles/PlainTiles.tsx
+++ packages/scrabble-solver/src/components/PlainTiles/PlainTiles.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, FunctionComponent } from 'react';
+import React, { CSSProperties, FunctionComponent, useMemo } from 'react';
 
 import { createTiles, getViewbox } from './lib';
 import Tile from './Tile';
@@ -6,24 +6,29 @@ import Tile from './Tile';
 interface Props {
   className?: string;
   content: string[][];
+  floating?: boolean;
   style?: CSSProperties;
 }
 
-const PlainTiles: FunctionComponent<Props> = ({ className, content, style }) => (
-  <svg className={className} style={style} viewBox={getViewbox(content)} xmlns="http://www.w3.org/2000/svg">
-    {createTiles(content).map((tile, index) => (
-      <Tile
-        character={tile.character}
-        color={tile.color}
-        key={index}
-        points={tile.points}
-        size={tile.size}
-        transform={tile.transform}
-        x={tile.x}
-        y={tile.y}
-      />
-    ))}
-  </svg>
-);
+const PlainTiles: FunctionComponent<Props> = ({ className, content, style }) => {
+  const tiles = useMemo(() => createTiles(content), [content]);
+
+  return (
+    <svg className={className} style={style} viewBox={getViewbox(content)} xmlns="http://www.w3.org/2000/svg">
+      {tiles.map((tile, index) => (
+        <Tile
+          character={tile.character}
+          color={tile.color}
+          key={index}
+          points={tile.points}
+          size={tile.size}
+          transform={tile.transform}
+          x={tile.x}
+          y={tile.y}
+        />
+      ))}
+    </svg>
+  );
+};
 
 export default PlainTiles;
