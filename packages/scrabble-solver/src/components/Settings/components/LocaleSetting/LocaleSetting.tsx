45c52183c8a41a819a73dccde9c5e9e1a646e99e
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-08 12:40:10 +0700

    Inline LocaleOption into LocaleSetting

--- packages/scrabble-solver/src/components/Settings/components/LocaleSetting/LocaleSetting.tsx
+++ packages/scrabble-solver/src/components/Settings/components/LocaleSetting/LocaleSetting.tsx
@@ -1,7 +1,13 @@
 import classNames from 'classnames';
-import React, { FunctionComponent } from 'react';
+import React, { ChangeEvent, FunctionComponent } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { LocaleOption } from './components';
+import { i18nSlice, selectLocale, useTypedSelector } from 'state';
+import { Locale } from 'types';
+
+import Radio from '../../../Radio';
+import SvgIcon from '../../../SvgIcon';
+
 import styles from './LocaleSetting.module.scss';
 import options from './options';
 
@@ -9,12 +15,39 @@ interface Props {
   className?: string;
 }
 
-const LocaleSetting: FunctionComponent<Props> = ({ className }) => (
-  <div className={classNames(styles.localeSetting, className)}>
-    {options.map((option) => (
-      <LocaleOption className={styles.option} key={option.value} option={option} />
-    ))}
-  </div>
-);
+const LocaleSetting: FunctionComponent<Props> = ({ className }) => {
+  const dispatch = useDispatch();
+  const locale = useTypedSelector(selectLocale);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newLocale: Locale = event.target.value as Locale;
+    dispatch(i18nSlice.actions.changeLocale(newLocale));
+  };
+
+  return (
+    <div className={classNames(styles.localeSetting, className)}>
+      {options.map((option) => (
+        <Radio
+          checked={locale === option.value}
+          className={classNames(styles.option, className, {
+            [styles.checked]: locale === option.value,
+          })}
+          id="locale"
+          key={option.value}
+          name="locale"
+          title={option.label}
+          value={option.value}
+          onChange={handleChange}
+        >
+          <div className={styles.label}>
+            <SvgIcon className={classNames(styles.flag, option.className)} icon={option.icon} />
+
+            <div>{option.label}</div>
+          </div>
+        </Radio>
+      ))}
+    </div>
+  );
+};
 
 export default LocaleSetting;
