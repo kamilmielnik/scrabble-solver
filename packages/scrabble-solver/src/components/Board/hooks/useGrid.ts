ff4815942043597b33b28d5cb4bef84bb0262055
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-12 21:18:46 +0700

    Ensure everything is memoized in useGrid

--- packages/scrabble-solver/src/components/Board/hooks/useGrid.ts
+++ packages/scrabble-solver/src/components/Board/hooks/useGrid.ts
@@ -1,4 +1,5 @@
 import { createRef, KeyboardEventHandler, RefObject, useCallback, useMemo, useState, useRef } from 'react';
+import { useLatest } from 'react-use';
 
 import { createKeyboardNavigation, isCtrl } from 'lib';
 
@@ -26,67 +27,60 @@ const useGrid = ({ height, width }: Parameters): [State, Actions] => {
     () => createGridOf<RefObject<HTMLInputElement>>(width, height, () => createRef()),
     [width, height],
   );
-  const activeIndex = useRef<{ x: number | null; y: number | null }>({ x: null, y: null });
+  const activeIndexRef = useRef<{ x: number | null; y: number | null }>({ x: null, y: null });
   const [lastDirection, setLastDirection] = useState<'horizontal' | 'vertical'>('horizontal');
+  const lastDirectionRef = useLatest(lastDirection);
 
-  const changeActiveIndex = useCallback(
-    (offsetX, offsetY) => {
-      const x = Math.min(Math.max(activeIndex.current.x + offsetX, 0), width - 1);
-      const y = Math.min(Math.max(activeIndex.current.y + offsetY, 0), height - 1);
-      activeIndex.current = { x, y };
-      refs[y][x].current?.focus();
-    },
-    [activeIndex, height, refs, width],
-  );
+  const changeActiveIndexRef = useLatest((offsetX, offsetY) => {
+    const x = Math.min(Math.max(activeIndexRef.current.x + offsetX, 0), width - 1);
+    const y = Math.min(Math.max(activeIndexRef.current.y + offsetY, 0), height - 1);
+    activeIndexRef.current = { x, y };
+    refs[y][x].current?.focus();
+  });
 
-  const onDirectionToggle = useCallback(
-    () =>
-      setLastDirection((direction) => {
-        return direction === 'vertical' ? 'horizontal' : 'vertical';
-      }),
-    [],
-  );
+  const onDirectionToggle = useCallback(() => {
+    setLastDirection((direction) => {
+      return direction === 'vertical' ? 'horizontal' : 'vertical';
+    });
+  }, []);
 
-  const onFocus = useCallback(
-    (x, y) => {
-      activeIndex.current = { x, y };
-    },
-    [activeIndex],
-  );
+  const onFocus = useCallback((x, y) => {
+    activeIndexRef.current = { x, y };
+  }, []);
 
   const onMoveFocus = useCallback(() => {
-    if (lastDirection === 'horizontal') {
-      changeActiveIndex(1, 0);
-    } else if (lastDirection === 'vertical') {
-      changeActiveIndex(0, 1);
+    if (lastDirectionRef.current === 'horizontal') {
+      changeActiveIndexRef.current(1, 0);
+    } else if (lastDirectionRef.current === 'vertical') {
+      changeActiveIndexRef.current(0, 1);
     }
-  }, [changeActiveIndex, lastDirection]);
+  }, [changeActiveIndexRef, lastDirectionRef]);
 
   const onKeyDown = useMemo(
     () =>
       createKeyboardNavigation({
         onArrowDown: (event) => {
           if (!isCtrl(event)) {
-            changeActiveIndex(0, 1);
+            changeActiveIndexRef.current(0, 1);
           }
         },
         onArrowLeft: (event) => {
           if (!isCtrl(event)) {
-            changeActiveIndex(-1, 0);
+            changeActiveIndexRef.current(-1, 0);
           }
         },
         onArrowRight: (event) => {
           if (!isCtrl(event)) {
-            changeActiveIndex(1, 0);
+            changeActiveIndexRef.current(1, 0);
           }
         },
         onArrowUp: (event) => {
           if (!isCtrl(event)) {
-            changeActiveIndex(0, -1);
+            changeActiveIndexRef.current(0, -1);
           }
         },
       }),
-    [changeActiveIndex],
+    [changeActiveIndexRef],
   );
 
   return [
