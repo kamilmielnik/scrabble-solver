05a1466ecaf3c354e8071c3a675ad0af884e0954
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-12 21:27:36 +0700

    Format code

--- packages/scrabble-solver/src/components/Board/hooks/useGrid.ts
+++ packages/scrabble-solver/src/components/Board/hooks/useGrid.ts
@@ -27,11 +27,11 @@ const useGrid = ({ height, width }: Parameters): [State, Actions] => {
     () => createGridOf<RefObject<HTMLInputElement>>(width, height, () => createRef()),
     [width, height],
   );
-  const activeIndexRef = useRef<{ x: number | null; y: number | null }>({ x: null, y: null });
+  const activeIndexRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
   const [lastDirection, setLastDirection] = useState<'horizontal' | 'vertical'>('horizontal');
   const lastDirectionRef = useLatest(lastDirection);
 
-  const changeActiveIndexRef = useLatest((offsetX, offsetY) => {
+  const changeActiveIndexRef = useLatest((offsetX: number, offsetY: number) => {
     const x = Math.min(Math.max(activeIndexRef.current.x + offsetX, 0), width - 1);
     const y = Math.min(Math.max(activeIndexRef.current.y + offsetY, 0), height - 1);
     activeIndexRef.current = { x, y };
@@ -56,40 +56,38 @@ const useGrid = ({ height, width }: Parameters): [State, Actions] => {
     }
   }, [changeActiveIndexRef, lastDirectionRef]);
 
-  const onKeyDown = useMemo(
-    () =>
-      createKeyboardNavigation({
-        onArrowDown: (event) => {
-          if (isCtrl(event)) {
-            setLastDirection('vertical');
-          } else {
-            changeActiveIndexRef.current(0, 1);
-          }
-        },
-        onArrowLeft: (event) => {
-          if (isCtrl(event)) {
-            setLastDirection('horizontal');
-          } else {
-            changeActiveIndexRef.current(-1, 0);
-          }
-        },
-        onArrowRight: (event) => {
-          if (isCtrl(event)) {
-            setLastDirection('horizontal');
-          } else {
-            changeActiveIndexRef.current(1, 0);
-          }
-        },
-        onArrowUp: (event) => {
-          if (isCtrl(event)) {
-            setLastDirection('vertical');
-          } else {
-            changeActiveIndexRef.current(0, -1);
-          }
-        },
-      }),
-    [changeActiveIndexRef],
-  );
+  const onKeyDown = useMemo(() => {
+    return createKeyboardNavigation({
+      onArrowDown: (event) => {
+        if (isCtrl(event)) {
+          setLastDirection('vertical');
+        } else {
+          changeActiveIndexRef.current(0, 1);
+        }
+      },
+      onArrowLeft: (event) => {
+        if (isCtrl(event)) {
+          setLastDirection('horizontal');
+        } else {
+          changeActiveIndexRef.current(-1, 0);
+        }
+      },
+      onArrowRight: (event) => {
+        if (isCtrl(event)) {
+          setLastDirection('horizontal');
+        } else {
+          changeActiveIndexRef.current(1, 0);
+        }
+      },
+      onArrowUp: (event) => {
+        if (isCtrl(event)) {
+          setLastDirection('vertical');
+        } else {
+          changeActiveIndexRef.current(0, -1);
+        }
+      },
+    });
+  }, [changeActiveIndexRef]);
 
   return [
     { lastDirection, refs },
