5b134dde53e4d0a41090d86ad2d238ccba617b7f
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-15 01:22:20 +0700

    Add logging in dictionary input

--- packages/scrabble-solver/src/pages/api/dictionary/[locale]/[word].ts
+++ packages/scrabble-solver/src/pages/api/dictionary/[locale]/[word].ts
@@ -1,8 +1,14 @@
+import logger from '@scrabble-solver/logger';
 import { WordDefinition } from '@scrabble-solver/models';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { validateLocale, validateWord, translateEn, translatePl } from 'api';
 import { Locale } from 'types';
+
+interface RequestData {
+  locale: Locale;
+  word: string;
+}
 
 const localeTranslate: Record<Locale, (word: string) => Promise<WordDefinition>> = {
   'en-GB': translateEn,
@@ -12,16 +18,22 @@ const localeTranslate: Record<Locale, (word: string) => Promise<WordDefinition>>
 
 const dictionary = async (request: NextApiRequest, response: NextApiResponse): Promise<void> => {
   try {
-    const { locale, word } = parseRequest(request);
+    const requestData = parseRequest(request);
+    logRequest(requestData);
+    const { locale, word } = requestData;
     const translate = localeTranslate[locale];
     const result = await translate(word);
     response.status(200).send(result.toJson());
   } catch (error) {
-    response.status(500).send('Server error');
+    logger.error(error);
+    response.status(500).send({
+      error: 'Server error',
+      message: error.message,
+    });
   }
 };
 
-const parseRequest = (request: NextApiRequest): { locale: Locale; word: string } => {
+const parseRequest = (request: NextApiRequest): RequestData => {
   const { locale, word } = request.query;
 
   validateLocale(locale);
@@ -31,6 +43,15 @@ const parseRequest = (request: NextApiRequest): { locale: Locale; word: string }
     locale: locale as Locale,
     word: word as string,
   };
+};
+
+const logRequest = (requestData: RequestData) => {
+  const { locale, word } = requestData;
+
+  logger.info('dictionary - request data', {
+    locale,
+    word,
+  });
 };
 
 export default dictionary;
