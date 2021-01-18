8358847c85417422daac2f9791b33a78f596e08a
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-19 02:53:40 +0700

    Replace JS animation with CSS animation for performance

--- packages/scrabble-solver/src/components/Splash/Splash.tsx
+++ packages/scrabble-solver/src/components/Splash/Splash.tsx
@@ -7,8 +7,6 @@ import Screen from '../Screen';
 
 import styles from './Splash.module.scss';
 
-const COMPLETE_SPLASH_DURATION_PERCENT = 0.15;
-
 interface Props {
   className?: string;
   duration: number;
@@ -18,8 +16,6 @@ interface Props {
 const Splash: FunctionComponent<Props> = ({ className, duration, forceShow }) => {
   const [isTimeoutFinished, setIsTimeoutFinished] = useState<boolean>(false);
   const hidden = isTimeoutFinished && !forceShow;
-  const progress = useTween('inCubic', (1 - COMPLETE_SPLASH_DURATION_PERCENT) * duration);
-  const progressPercent = `${(progress * 100).toFixed(2)}%`;
 
   useEffect(() => {
     const timeout = window.setTimeout(() => {
@@ -33,18 +29,14 @@ const Splash: FunctionComponent<Props> = ({ className, duration, forceShow }) =>
 
   return (
     <Screen className={classNames(styles.splash, className)} hidden={hidden}>
-      <Logo
-        className={classNames(styles.logo, styles.logoGrayscale, {
+      <div
+        className={classNames(styles.logos, {
           [styles.pulsating]: isTimeoutFinished,
         })}
-      />
-
-      <Logo
-        className={classNames(styles.logo, styles.logoColor, {
-          [styles.pulsating]: isTimeoutFinished,
-        })}
-        style={{ clipPath: `polygon(0% 0%, ${progressPercent} 0, ${progressPercent} 100%, 0% 100%)` }}
-      />
+      >
+        <Logo className={styles.logoGrayscale} />
+        <Logo className={styles.logoColor} />
+      </div>
 
       <h1 className={styles.author}>by Kamil Mielnik</h1>
     </Screen>
