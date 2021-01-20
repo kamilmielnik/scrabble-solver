27676a746a6a4369dce1ac959c91295a07fa49db
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-21 00:28:21 +0700

    ESLint

--- packages/scrabble-solver/src/lib/getTileSizes.ts
+++ packages/scrabble-solver/src/lib/getTileSizes.ts
@@ -1,6 +1,12 @@
 import { BOARD_TILE_FONT_SIZE_MIN, BOARD_TILE_FONT_SIZE_POINTS_MIN } from 'const';
 
-const getTileSizes = (tileSize: number) => ({
+interface TileSizes {
+  pointsFontSize: number;
+  tileFontSize: number;
+  tileSize: number;
+}
+
+const getTileSizes = (tileSize: number): TileSizes => ({
   pointsFontSize: Math.max(Math.round(tileSize * 0.25), BOARD_TILE_FONT_SIZE_POINTS_MIN),
   tileFontSize: Math.max(Math.round(tileSize * 0.6), BOARD_TILE_FONT_SIZE_MIN),
   tileSize,
