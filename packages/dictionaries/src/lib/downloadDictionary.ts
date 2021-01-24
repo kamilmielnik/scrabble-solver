53992ac754d79d91e8843f253f5a45b26e6256f1
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-25 02:59:44 +0700

    Add logging to @scrabble-solver/dictionaries

--- packages/dictionaries/src/lib/downloadDictionary.ts
+++ packages/dictionaries/src/lib/downloadDictionary.ts
@@ -1,9 +1,12 @@
 import { Trie } from '@kamilmielnik/trie';
+import logger from '@scrabble-solver/logger';
 import { Locale } from '@scrabble-solver/types';
 import { getWordList } from '@scrabble-solver/word-lists';
 
 const downloadDictionary = async (locale: Locale): Promise<Trie> => {
+  logger.info('downloadDictionary', { locale });
   const words = await getWordList(locale);
+  logger.info('downloadDictionary - success', { locale });
   const trie = Trie.fromArray(words);
   return trie;
 };
