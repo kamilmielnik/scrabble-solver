9696ce4f08bbe60cc0893679f5b24ad2a802b386
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-24 20:49:06 +0700

    Move test cases data to files

--- packages/word-definitions/src/getWordDefinition.ts
+++ packages/word-definitions/src/getWordDefinition.ts
@@ -11,25 +11,26 @@ const getWordDefinition = async (word: string): Promise<WordDefinition> => {
     hostname: `${locale}.wiktionary.org`,
     path: `/w/api.php?action=query&format=json&prop=extracts&titles=${encodeURIComponent(word)}`,
   });
-  const html = parseResponse(response);
-  const wordDefinition = parseHtml(html, word);
+  const wordDefinition = parseResponse(response, word);
   return wordDefinition;
 };
 
-const parseResponse = (response: string): string => {
+export const parseResponse = (response: string, word: string): WordDefinition => {
   const json = JSON.parse(response) as WiktionaryResponse;
   const pages = json.query.pages;
   const keys = Object.keys(pages);
   const key = keys[0];
 
   if (typeof key === 'undefined') {
-    return '';
+    return new WordDefinition({ definitions: [], isAllowed: false, word });
   }
 
-  return pages[key].extract;
+  const html = pages[key].extract;
+  const wordDefinition = parseHtml(html, word);
+  return wordDefinition;
 };
 
-export const parseHtml = (html: string, word: string): WordDefinition => {
+const parseHtml = (html: string, word: string): WordDefinition => {
   const $ = cheerio.load(html);
   const $definitions = $('h4 + p + ol:first-of-type > li');
   $('h4 + p + ol:first-of-type > li ul,ol,dl,.HQToggle').remove();
