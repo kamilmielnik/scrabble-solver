5c1487fc80a25e303eee9099daa395031f3ed7c4
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-27 02:23:18 +0700

    Fix cell sizing on some dimensions

--- packages/scrabble-solver/src/lib/getCellSize.ts
+++ packages/scrabble-solver/src/lib/getCellSize.ts
@@ -6,7 +6,7 @@ const getCellSize = (config: Config, width: number, height: number): number => {
   const maxWidth = (width - BOARD_CELL_BORDER_WIDTH) / config.boardWidth - BOARD_CELL_BORDER_WIDTH;
   const maxHeight = (height - BOARD_CELL_BORDER_WIDTH) / config.boardHeight - BOARD_CELL_BORDER_WIDTH;
   const cellSize = Math.min(maxWidth, maxHeight);
-  return Math.min(Math.max(cellSize, BOARD_TILE_SIZE_MIN), BOARD_TILE_SIZE_MAX);
+  return Math.floor(Math.min(Math.max(cellSize, BOARD_TILE_SIZE_MIN), BOARD_TILE_SIZE_MAX));
 };
 
 export default getCellSize;
