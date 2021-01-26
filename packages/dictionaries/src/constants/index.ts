8f563dd23a2bebe0d0e42c75320989bcac4cfa20
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-27 01:31:12 +0700

    Make sure the dictionaries directory is consistent regardless of where the code is run from

--- packages/dictionaries/src/constants/index.ts
+++ packages/dictionaries/src/constants/index.ts
@@ -1,5 +1,8 @@
+import os from 'os';
+import path from 'path';
+
 export const DAY = 24 * 60 * 60 * 1000;
 
 export const CACHE_STALE_THRESHOLD = DAY;
 
-export const OUTPUT_DIRECTORY = 'dictionaries';
+export const OUTPUT_DIRECTORY = path.resolve(os.homedir(), '.scrabble-solver', 'dictionaries');
