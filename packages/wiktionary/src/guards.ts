46bfa2ae5e883326c0b74b31788e489b7fc136e9
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-29 00:54:05 +0700

    ESLint

--- packages/wiktionary/src/guards.ts
+++ packages/wiktionary/src/guards.ts
@@ -1,10 +1,10 @@
 import { MissingPage, Page, WiktionaryResponse } from './types';
 
-const isObject = (value: any): value is Record<string, unknown> => {
+const isObject = (value: unknown): value is Record<string, unknown> => {
   return typeof value === 'object' && value !== null;
 };
 
-export const isMissingPage = (value: any): value is MissingPage => {
+export const isMissingPage = (value: unknown): value is MissingPage => {
   return (
     isObject(value) &&
     typeof value.missing === 'string' &&
@@ -13,7 +13,7 @@ export const isMissingPage = (value: any): value is MissingPage => {
   );
 };
 
-export const isPage = (value: any): value is Page => {
+export const isPage = (value: unknown): value is Page => {
   return (
     isObject(value) &&
     typeof value.extract === 'string' &&
@@ -23,22 +23,22 @@ export const isPage = (value: any): value is Page => {
   );
 };
 
-export const isPages = (value: any): value is WiktionaryResponse['query']['pages'] => {
-  return isObject(value) && Object.values(value).every((value) => isPage(value) || isMissingPage(value));
+export const isPages = (value: unknown): value is WiktionaryResponse['query']['pages'] => {
+  return isObject(value) && Object.values(value).every((pageValue) => isPage(pageValue) || isMissingPage(pageValue));
 };
 
-export const isQuery = (value: any): value is WiktionaryResponse['query'] => {
+export const isQuery = (value: unknown): value is WiktionaryResponse['query'] => {
   return isObject(value) && isPages(value.pages);
 };
 
-export const isWarnings = (value: any): value is WiktionaryResponse['warnings'] => {
+export const isWarnings = (value: unknown): value is WiktionaryResponse['warnings'] => {
   return (
     typeof value === 'undefined' ||
     (isObject(value) && isObject(value.extracts) && typeof value.extracts['*'] === 'string')
   );
 };
 
-export const isWiktionaryResponse = (value: any): value is WiktionaryResponse => {
+export const isWiktionaryResponse = (value: unknown): value is WiktionaryResponse => {
   return (
     isObject(value) && typeof value.batchcomplete === 'string' && isWarnings(value.warnings) && isQuery(value.query)
   );
