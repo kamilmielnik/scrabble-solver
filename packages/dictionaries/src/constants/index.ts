2d044d167bf2004fcf9d8224e3d7e6a6ea620ceb
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-01-29 00:52:17 +0700

    Increase CACHE_STALE_THRESHOLD to 30 days

--- packages/dictionaries/src/constants/index.ts
+++ packages/dictionaries/src/constants/index.ts
@@ -3,6 +3,6 @@ import path from 'path';
 
 export const DAY = 24 * 60 * 60 * 1000;
 
-export const CACHE_STALE_THRESHOLD = DAY;
+export const CACHE_STALE_THRESHOLD = 30 * DAY;
 
 export const OUTPUT_DIRECTORY = path.resolve(os.homedir(), '.scrabble-solver', 'dictionaries');
