3d15f5a5c22ba4bdd4b586fe43fad32350a79848
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-02-04 11:58:31 +0700

    Refactor Translations type

--- packages/scrabble-solver/src/types/index.ts
+++ packages/scrabble-solver/src/types/index.ts
@@ -1,46 +1,47 @@
 export type Comparator<T> = (a: T, B: T) => number;
 
-export type Translate = (key: keyof Translations) => string;
+export type TranslationKey =
+  | 'cell.set-blank'
+  | 'cell.set-not-blank'
+  | 'cell.toggle-direction'
+  | 'clear'
+  | 'close'
+  | 'dictionary.empty-state.no-definitions'
+  | 'dictionary.empty-state.no-results'
+  | 'dictionary.empty-state.not-allowed'
+  | 'dictionary.empty-state.unitialized'
+  | 'empty-state.error'
+  | 'empty-state.info'
+  | 'empty-state.success'
+  | 'empty-state.warning'
+  | 'github'
+  | 'keyMap'
+  | 'keyMap.board'
+  | 'keyMap.board.toggle-blank'
+  | 'keyMap.board.toggle-direction'
+  | 'keyMap.board-and-rack'
+  | 'keyMap.board-and-rack.navigate'
+  | 'keyMap.board-and-rack.remove-tile'
+  | 'keyMap.board-and-rack.submit'
+  | 'keyMap.rack'
+  | 'keyMap.rack.insert-blank'
+  | 'loading'
+  | 'rack.placeholder'
+  | 'remaining-tiles'
+  | 'results.empty-state.outdated'
+  | 'results.empty-state.no-results'
+  | 'results.empty-state.unitialized'
+  | 'results.header.points'
+  | 'results.header.word'
+  | 'results.solve'
+  | 'settings'
+  | 'settings.autoGroupTiles'
+  | 'settings.autoGroupTiles.left'
+  | 'settings.autoGroupTiles.right'
+  | 'settings.autoGroupTiles.null'
+  | 'settings.game'
+  | 'settings.language';
 
-export interface Translations {
-  'cell.set-blank': string;
-  'cell.set-not-blank': string;
-  'cell.toggle-direction': string;
-  clear: string;
-  close: string;
-  'dictionary.empty-state.no-definitions': string;
-  'dictionary.empty-state.no-results': string;
-  'dictionary.empty-state.not-allowed': string;
-  'dictionary.empty-state.unitialized': string;
-  'empty-state.error': string;
-  'empty-state.info': string;
-  'empty-state.success': string;
-  'empty-state.warning': string;
-  github: string;
-  keyMap: string;
-  'keyMap.board': string;
-  'keyMap.board.toggle-blank': string;
-  'keyMap.board.toggle-direction': string;
-  'keyMap.board-and-rack': string;
-  'keyMap.board-and-rack.navigate': string;
-  'keyMap.board-and-rack.remove-tile': string;
-  'keyMap.board-and-rack.submit': string;
-  'keyMap.rack': string;
-  'keyMap.rack.insert-blank': string;
-  loading: string;
-  'rack.placeholder': string;
-  'remaining-tiles': string;
-  'results.empty-state.outdated': string;
-  'results.empty-state.no-results': string;
-  'results.empty-state.unitialized': string;
-  'results.header.points': string;
-  'results.header.word': string;
-  'results.solve': string;
-  settings: string;
-  'settings.autoGroupTiles': string;
-  'settings.autoGroupTiles.left': string;
-  'settings.autoGroupTiles.right': string;
-  'settings.autoGroupTiles.null': string;
-  'settings.game': string;
-  'settings.language': string;
-}
+export type Translate = (key: TranslationKey) => string;
+
+export type Translations = Record<TranslationKey, string>;
