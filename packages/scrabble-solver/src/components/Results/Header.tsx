eb12ee188d38c75104c726382e1290badb532973
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-02-12 01:21:32 +0700

    Refactor markup duplication in Header

--- packages/scrabble-solver/src/components/Results/Header.tsx
+++ packages/scrabble-solver/src/components/Results/Header.tsx
@@ -2,33 +2,54 @@ import classNames from 'classnames';
 import React, { ReactElement } from 'react';
 
 import { useTranslate } from 'state';
+import { TranslationKey } from 'types';
 
 import styles from './Results.module.scss';
+
+interface Column {
+  className: string;
+  labelKey: TranslationKey;
+  titleKey: TranslationKey;
+}
+
+const columns: Column[] = [
+  {
+    className: styles.word,
+    labelKey: 'results.header.word',
+    titleKey: 'results.header.word',
+  },
+  {
+    className: styles.stat,
+    labelKey: 'results.header.tiles.short',
+    titleKey: 'results.header.tiles',
+  },
+  {
+    className: styles.stat,
+    labelKey: 'results.header.blanks.short',
+    titleKey: 'results.header.blanks',
+  },
+  {
+    className: styles.stat,
+    labelKey: 'results.header.words.short',
+    titleKey: 'results.header.words',
+  },
+  {
+    className: styles.points,
+    labelKey: 'results.header.points',
+    titleKey: 'results.header.points',
+  },
+];
 
 const Header = (): ReactElement => {
   const translate = useTranslate();
 
   return (
     <div className={styles.header}>
-      <div className={classNames(styles.cell, styles.word)} title={translate('results.header.word')}>
-        {translate('results.header.word')}
-      </div>
-
-      <div className={classNames(styles.cell, styles.stat)} title={translate('results.header.tiles')}>
-        {translate('results.header.tiles.short')}
-      </div>
-
-      <div className={classNames(styles.cell, styles.stat)} title={translate('results.header.blanks')}>
-        {translate('results.header.blanks.short')}
-      </div>
-
-      <div className={classNames(styles.cell, styles.stat)} title={translate('results.header.words')}>
-        {translate('results.header.words.short')}
-      </div>
-
-      <div className={classNames(styles.cell, styles.points)} title={translate('results.header.points')}>
-        {translate('results.header.points')}
-      </div>
+      {columns.map((column, index) => (
+        <div className={classNames(styles.cell, column.className)} key={index} title={translate(column.titleKey)}>
+          {translate(column.labelKey)}
+        </div>
+      ))}
     </div>
   );
 };
