03bc0e3e2c308ed4be52077cdca8f637799ed231
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-02-19 01:20:29 +0700

    Render groups in remaining tiles

--- packages/scrabble-solver/src/components/RemainingTiles/RemainingTiles.tsx
+++ packages/scrabble-solver/src/components/RemainingTiles/RemainingTiles.tsx
@@ -37,42 +37,42 @@ const RemainingTiles: FunctionComponent<Props> = ({ className, isOpen, onClose }
   const vowels = remainingTiles.filter(({ character }) => VOWELS.includes(character));
   const blanks = remainingTiles.filter(({ character }) => character === BLANK);
 
-  return (
-    <Sidebar className={className} isOpen={isOpen} title={translate('remaining-tiles')} onClose={onClose}>
-      <div className={styles.summary}>
-        <div className={styles.entry} title={`${totalRemainingCount} / ${totalCount}`}>
-          <div className={styles.title}>{translate('results.header.tiles')}</div>
-          <div className={styles.value}>
-            {totalRemainingCount} / {totalCount}
-          </div>
-        </div>
-        <div className={styles.entry} title={`${getRemainingCount(consonants)} / ${getTotalCount(consonants)}`}>
-          <div className={styles.title}>{translate('results.header.consonants')}</div>
-          <div className={styles.value}>
-            {getRemainingCount(consonants)} / {getTotalCount(consonants)}
-          </div>
-        </div>
-        <div className={styles.entry} title={`${getRemainingCount(vowels)} / ${getTotalCount(vowels)}`}>
-          <div className={styles.title}>{translate('results.header.vowels')}</div>
-          <div className={styles.value}>
-            {getRemainingCount(vowels)} / {getTotalCount(vowels)}
-          </div>
-        </div>
-        <div className={styles.entry} title={`${getRemainingCount(blanks)} / ${getTotalCount(blanks)}`}>
-          <div className={styles.title}>{translate('results.header.blanks')}</div>
-          <div className={styles.value}>
-            {getRemainingCount(blanks)} / {getTotalCount(blanks)}
-          </div>
-        </div>
-      </div>
+  const groups = [
+    {
+      tiles: vowels,
+      title: translate('results.header.vowels'),
+    },
+    {
+      tiles: consonants,
+      title: translate('results.header.consonants'),
+    },
+    {
+      tiles: blanks,
+      title: translate('results.header.blanks'),
+    },
+  ];
 
-      <div
-        className={styles.content}
-        title={`${translate('remaining-tiles')} (${totalRemainingCount} / ${totalCount})`}
-      >
-        {remainingTiles.map(({ character, count, usedCount }) => {
-          return <Character character={character} count={count} key={character} usedCount={usedCount} />;
-        })}
+  return (
+    <Sidebar
+      className={className}
+      isOpen={isOpen}
+      title={`${translate('remaining-tiles')} (${totalRemainingCount})`}
+      onClose={onClose}
+    >
+      <div title={`${translate('remaining-tiles')} (${totalRemainingCount} / ${totalCount})`}>
+        {groups.map(({ tiles, title }) => (
+          <div className={styles.group} key={title}>
+            <h2 className={styles.title}>
+              {title} ({getRemainingCount(tiles)} / {getTotalCount(tiles)})
+            </h2>
+
+            <div className={styles.content}>
+              {tiles.map(({ character, count, usedCount }) => {
+                return <Character character={character} count={count} key={character} usedCount={usedCount} />;
+              })}
+            </div>
+          </div>
+        ))}
       </div>
     </Sidebar>
   );
