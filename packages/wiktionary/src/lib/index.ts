ea064526f345e8daa5b63ed0c95572ed272f4c77
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-02-20 02:10:49 +0700

    Remove @scrabble-solver/wiktionary package

--- packages/wiktionary/src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as normalizeDefinition } from './normalizeDefinition';
-export { default as request } from './request';
-export { default as unique } from './unique';
--- packages/wiktionary/src/lib/unique.ts
+++
@@ -1,3 +0,0 @@
-const unique = <T>(array: T[]): T[] => Array.from(new Set(array));
-
-export default unique;
