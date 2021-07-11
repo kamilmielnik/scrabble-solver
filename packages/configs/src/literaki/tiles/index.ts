d9220bae07f030f7ebb2912d1d4e5dadb0e1542b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-11 17:22:55 +0700

    Add Spanish configs for Literaki & Scrabble

--- packages/configs/src/literaki/tiles/index.ts
+++ packages/configs/src/literaki/tiles/index.ts
@@ -1,3 +1,4 @@
 export { default as tilesEn } from './tilesEn';
+export { default as tilesEs } from './tilesEs';
 export { default as tilesFr } from './tilesFr';
 export { default as tilesPl } from './tilesPl';
