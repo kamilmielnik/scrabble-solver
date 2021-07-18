57534c2c92a183aae9cbf89abc0d410d58068f1e
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-18 17:41:03 +0700

    Add Tooltip to Button

--- packages/scrabble-solver/src/components/Button/Button.tsx
+++ packages/scrabble-solver/src/components/Button/Button.tsx
@@ -1,5 +1,9 @@
 import classNames from 'classnames';
-import React, { ButtonHTMLAttributes, FunctionComponent, MouseEventHandler } from 'react';
+import React, { ButtonHTMLAttributes, FunctionComponent, MouseEventHandler, ReactNode } from 'react';
+
+import { noop } from 'lib';
+
+import Tooltip from '../Tooltip';
 
 import styles from './Button.module.scss';
 import Content from './Content';
@@ -8,12 +12,49 @@ import Link from './Link';
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon: BrowserSpriteSymbol;
   onClick: MouseEventHandler<HTMLButtonElement>;
+  tooltip?: ReactNode;
 }
 
-const Button: FunctionComponent<Props> = ({ children, className, icon, ...props }) => (
-  <button className={classNames(styles.button, className)} type="button" {...props}>
-    <Content icon={icon}>{children}</Content>
-  </button>
+const Button: FunctionComponent<Props> = ({
+  children,
+  className,
+  icon,
+  tooltip,
+  onBlur = noop,
+  onFocus = noop,
+  onMouseOut = noop,
+  onMouseOver = noop,
+  ...props
+}) => (
+  <Tooltip tooltip={tooltip}>
+    {({ ariaAttributes, setReferenceElement, onHide, onShow }) => (
+      <button
+        {...ariaAttributes}
+        className={classNames(styles.button, className)}
+        ref={setReferenceElement}
+        type="button"
+        onBlur={(event) => {
+          onHide();
+          onBlur(event);
+        }}
+        onFocus={(event) => {
+          onShow();
+          onFocus(event);
+        }}
+        onMouseOut={(event) => {
+          onHide();
+          onMouseOut(event);
+        }}
+        onMouseOver={(event) => {
+          onShow();
+          onMouseOver(event);
+        }}
+        {...props}
+      >
+        <Content icon={icon}>{children}</Content>
+      </button>
+    )}
+  </Tooltip>
 );
 
 export default Object.assign(Button, {
