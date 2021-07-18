a2e38a34e2e2aec11e4b877a7940dd54348a580b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-19 01:22:35 +0700

    Use useTooltip in Button

--- packages/scrabble-solver/src/components/Button/Button.tsx
+++ packages/scrabble-solver/src/components/Button/Button.tsx
@@ -1,10 +1,8 @@
 import classNames from 'classnames';
 import React, { ButtonHTMLAttributes, FunctionComponent, MouseEventHandler, ReactNode } from 'react';
 
-import { noop } from 'lib';
-
 import SvgIcon from '../SvgIcon';
-import Tooltip from '../Tooltip';
+import { useTooltip } from '../Tooltip';
 
 import styles from './Button.module.scss';
 
@@ -14,49 +12,17 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   tooltip?: ReactNode;
 }
 
-const Button: FunctionComponent<Props> = ({
-  children,
-  className,
-  icon,
-  tooltip,
-  onBlur = noop,
-  onFocus = noop,
-  onMouseOut = noop,
-  onMouseOver = noop,
-  ...props
-}) => (
-  <Tooltip tooltip={tooltip}>
-    {({ ariaAttributes, setReferenceElement, onHide, onShow }) => (
-      <button
-        {...ariaAttributes}
-        className={classNames(styles.button, className)}
-        ref={setReferenceElement}
-        type="button"
-        onBlur={(event) => {
-          onHide();
-          onBlur(event);
-        }}
-        onFocus={(event) => {
-          onShow();
-          onFocus(event);
-        }}
-        onMouseOut={(event) => {
-          onHide();
-          onMouseOut(event);
-        }}
-        onMouseOver={(event) => {
-          onShow();
-          onMouseOver(event);
-        }}
-        {...props}
-      >
-        <span className={styles.content}>
-          <span className={styles.label}>{children}</span>
-          <SvgIcon className={styles.icon} icon={icon} />
-        </span>
-      </button>
-    )}
-  </Tooltip>
-);
+const Button: FunctionComponent<Props> = ({ children, className, icon, tooltip, ...props }) => {
+  const triggerProps = useTooltip(tooltip, props);
+
+  return (
+    <button className={classNames(styles.button, className)} type="button" {...triggerProps} {...props}>
+      <span className={styles.content}>
+        <span className={styles.label}>{children}</span>
+        <SvgIcon className={styles.icon} icon={icon} />
+      </span>
+    </button>
+  );
+};
 
 export default Button;
