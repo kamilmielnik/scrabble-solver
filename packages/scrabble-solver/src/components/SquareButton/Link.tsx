64b5e5e943393aca83c7ee53390dc5ebe07b985b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-18 16:45:29 +0700

    Add Tooltip to SquareButton.Link

--- packages/scrabble-solver/src/components/SquareButton/Link.tsx
+++ packages/scrabble-solver/src/components/SquareButton/Link.tsx
@@ -1,7 +1,11 @@
 import classNames from 'classnames';
 import React, { AnchorHTMLAttributes, FunctionComponent } from 'react';
 
-import Content from './Content';
+import { noop } from 'lib';
+
+import SvgIcon from '../SvgIcon';
+import Tooltip from '../Tooltip';
+
 import styles from './SquareButton.module.scss';
 
 interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
@@ -10,10 +14,48 @@ interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
   title: string;
 }
 
-const Link: FunctionComponent<Props> = ({ children, className, icon, ...props }) => (
-  <a className={classNames(styles.squareButton, className)} {...props}>
-    <Content icon={icon}>{children}</Content>
-  </a>
+const Link: FunctionComponent<Props> = ({
+  children,
+  className,
+  icon,
+  onBlur = noop,
+  onFocus = noop,
+  onMouseOut = noop,
+  onMouseOver = noop,
+  title,
+  ...props
+}) => (
+  <Tooltip tooltip={title}>
+    {({ ariaAttributes, setReferenceElement, onHide, onShow }) => (
+      <a
+        {...ariaAttributes}
+        className={classNames(styles.squareButton, className)}
+        ref={setReferenceElement}
+        type="button"
+        onBlur={(event) => {
+          onHide();
+          onBlur(event);
+        }}
+        onFocus={(event) => {
+          onShow();
+          onFocus(event);
+        }}
+        onMouseOut={(event) => {
+          onHide();
+          onMouseOut(event);
+        }}
+        onMouseOver={(event) => {
+          onShow();
+          onMouseOver(event);
+        }}
+        {...props}
+      >
+        <span className={styles.content}>
+          <SvgIcon className={styles.icon} icon={icon} />
+        </span>
+      </a>
+    )}
+  </Tooltip>
 );
 
 export default Link;
