35d27b587fb4821a9d8ff29a82a9c342de4597b5
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-19 01:21:26 +0700

    Use useTooltip in Cell/Button

--- packages/scrabble-solver/src/components/Board/components/Cell/Button.tsx
+++ packages/scrabble-solver/src/components/Board/components/Cell/Button.tsx
@@ -1,9 +1,7 @@
 import classNames from 'classnames';
 import React, { ButtonHTMLAttributes, FunctionComponent, MouseEventHandler, ReactNode } from 'react';
 
-import { noop } from 'lib';
-
-import Tooltip from '../../../Tooltip';
+import { useTooltip } from '../../../Tooltip';
 
 import styles from './Cell.module.scss';
 
@@ -12,48 +10,22 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   tooltip: ReactNode;
 }
 
-const Button: FunctionComponent<Props> = ({
-  children,
-  className,
-  tooltip,
-  onBlur = noop,
-  onFocus = noop,
-  onMouseOut = noop,
-  onMouseOver = noop,
-  ...props
-}) => (
-  <Tooltip tooltip={tooltip}>
-    {({ ariaAttributes, setReferenceElement, onHide, onShow }) => (
-      <button
-        {...ariaAttributes}
-        className={classNames(styles.action, className)}
-        ref={setReferenceElement}
-        // It's fine to make it not focusable with TAB from a11y point of view
-        // because alternative key combos are provided.
-        tabIndex={-1}
-        type="button"
-        onBlur={(event) => {
-          onHide();
-          onBlur(event);
-        }}
-        onFocus={(event) => {
-          onShow();
-          onFocus(event);
-        }}
-        onMouseOut={(event) => {
-          onHide();
-          onMouseOut(event);
-        }}
-        onMouseOver={(event) => {
-          onShow();
-          onMouseOver(event);
-        }}
-        {...props}
-      >
-        {children}
-      </button>
-    )}
-  </Tooltip>
-);
+const Button: FunctionComponent<Props> = ({ children, className, tooltip, ...props }) => {
+  const triggerProps = useTooltip(tooltip, props);
+
+  return (
+    <button
+      className={classNames(styles.action, className)}
+      // It's fine to make it not focusable with TAB from a11y point of view
+      // because alternative key combos are provided.
+      tabIndex={-1}
+      type="button"
+      {...triggerProps}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
