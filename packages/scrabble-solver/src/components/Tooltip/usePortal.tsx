82e7c5aa21187bd8805adfe3955917b560baeb3c
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-19 01:03:32 +0700

    Add tagName argument to usePortal

--- packages/scrabble-solver/src/components/Tooltip/usePortal.tsx
+++ packages/scrabble-solver/src/components/Tooltip/usePortal.tsx
@@ -4,10 +4,12 @@ import { Portal } from 'react-portal';
 
 import { noop } from 'lib';
 
+type TagName = Parameters<typeof document.createElement>[0];
+
 const canUseDom = typeof window !== 'undefined' && window.document && window.document.createElement;
 
-const usePortal = (children: ReactNode): void => {
-  const element = useMemo(() => document.createElement('div'), []);
+const usePortal = (children: ReactNode, tagName: TagName = 'div'): void => {
+  const element = useMemo(() => document.createElement(tagName), [tagName]);
 
   useLayoutEffect(() => {
     document.body.appendChild(element);
