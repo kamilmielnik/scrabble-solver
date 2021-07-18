f0e5346a3fc6d16b04364e2102a3b97c78d4c8a0
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2021-07-19 01:03:51 +0700

    Use useTooltip in SquareButton

--- packages/scrabble-solver/src/components/SquareButton/SquareButton.tsx
+++ packages/scrabble-solver/src/components/SquareButton/SquareButton.tsx
@@ -4,7 +4,7 @@ import React, { ButtonHTMLAttributes, FunctionComponent, MouseEventHandler } fro
 import { noop } from 'lib';
 
 import SvgIcon from '../SvgIcon';
-import Tooltip from '../Tooltip';
+import { useTooltip } from '../Tooltip';
 
 import Link from './Link';
 import styles from './SquareButton.module.scss';
@@ -25,39 +25,39 @@ const SquareButton: FunctionComponent<Props> = ({
   onMouseOut = noop,
   onMouseOver = noop,
   ...props
-}) => (
-  <Tooltip tooltip={tooltip}>
-    {({ ariaAttributes, setReferenceElement, onHide, onShow }) => (
-      <button
-        {...ariaAttributes}
-        className={classNames(styles.squareButton, className)}
-        ref={setReferenceElement}
-        type="button"
-        onBlur={(event) => {
-          onHide();
-          onBlur(event);
-        }}
-        onFocus={(event) => {
-          onShow();
-          onFocus(event);
-        }}
-        onMouseOut={(event) => {
-          onHide();
-          onMouseOut(event);
-        }}
-        onMouseOver={(event) => {
-          onShow();
-          onMouseOver(event);
-        }}
-        {...props}
-      >
-        <span className={styles.content}>
-          <SvgIcon className={styles.icon} icon={icon} />
-        </span>
-      </button>
-    )}
-  </Tooltip>
-);
+}) => {
+  const { ariaAttributes, onHide, onShow, setReferenceElement } = useTooltip(tooltip);
+
+  return (
+    <button
+      {...ariaAttributes}
+      className={classNames(styles.squareButton, className)}
+      ref={setReferenceElement}
+      type="button"
+      onBlur={(event) => {
+        onHide();
+        onBlur(event);
+      }}
+      onFocus={(event) => {
+        onShow();
+        onFocus(event);
+      }}
+      onMouseOut={(event) => {
+        onHide();
+        onMouseOut(event);
+      }}
+      onMouseOver={(event) => {
+        onShow();
+        onMouseOver(event);
+      }}
+      {...props}
+    >
+      <span className={styles.content}>
+        <SvgIcon className={styles.icon} icon={icon} />
+      </span>
+    </button>
+  );
+};
 
 export default Object.assign(SquareButton, {
   Link,
