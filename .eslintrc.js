d8468892ff118ba3c3772c801ad912ac5cda3a7d
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-09-21 19:15:36 +0700

    Simplify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
 
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 
   parserOptions: {
     ecmaVersion: 6,
