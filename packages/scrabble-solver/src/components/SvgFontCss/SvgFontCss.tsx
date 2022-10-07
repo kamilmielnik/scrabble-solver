c8931dc12ca4a7e7c04d721a7cfb94accf1f8b70
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-07 15:59:33 +0700

    ESLint

--- packages/scrabble-solver/src/components/SvgFontCss/SvgFontCss.tsx
+++ packages/scrabble-solver/src/components/SvgFontCss/SvgFontCss.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent } from 'react';
 
-const SvgFontCss: FunctionComponent = () => (
-  <style type="text/css">
-    {`@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Open+Sans:wght@400;700&family=Roboto+Mono&display=swap');`}
-    {`text {`}
-    {`  font-family: 'Open Sans';`}
-    {`}`}
-  </style>
-);
+// eslint-disable-next-line max-len
+const CSS = `@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Open+Sans:wght@400;700&family=Roboto+Mono&display=swap');
+
+text {
+  font-family: 'Open Sans';
+}`;
+
+const SvgFontCss: FunctionComponent = () => <style type="text/css">{CSS}</style>;
 
 export default SvgFontCss;
