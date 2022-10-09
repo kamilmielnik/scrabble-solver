ae6984ab462379fecf63ef1db87012215d270a98
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-09 14:21:33 +0700

    Fix next.js warning - `useMedia` When server side rendering, defaultState should be defined to prevent a hydration mismatches.

--- packages/scrabble-solver/src/hooks/useIsTablet.ts
+++ packages/scrabble-solver/src/hooks/useIsTablet.ts
@@ -1,8 +1,8 @@
 import { useMedia } from 'react-use';
 
 const useIsTablet = (): boolean => {
-  const isTabletHeight = useMedia('(max-height: 800px)');
-  const isTabletWidth = useMedia('(max-width: 1024px)');
+  const isTabletHeight = useMedia('(max-height: 800px)', false);
+  const isTabletWidth = useMedia('(max-width: 1024px)', false);
   const isTablet = isTabletHeight || isTabletWidth;
   return isTablet;
 };
