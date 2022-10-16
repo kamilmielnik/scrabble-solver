21141f82f948fab005832d3d2aba4d5fc77d8270
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-16 16:05:42 +0700

    Bring solver test coverage back to 100%

--- packages/solver/src/generateEndIndices.test.ts
+++ packages/solver/src/generateEndIndices.test.ts
@@ -6,6 +6,11 @@ describe('generateEndIndices', () => {
   const emptyCell: Cell = { hasTile: () => false } as Cell;
   const filledCell: Cell = { hasTile: () => true } as Cell;
 
+  it('returns empty array when no cells given', () => {
+    const cells: Cell[] = [];
+    expect(generateEndIndices(cells, 0).length).toBe(0);
+  });
+
   it('finds indices where a new word can end - XXOOOX', () => {
     const cells = [filledCell, filledCell, emptyCell, emptyCell, emptyCell, filledCell];
     expect(generateEndIndices(cells, 3)).toEqual([5]);
--- packages/solver/src/generateStartIndices.test.ts
+++ packages/solver/src/generateStartIndices.test.ts
@@ -6,6 +6,11 @@ describe('generateStartIndices', () => {
   const emptyCell: Cell = { hasTile: () => false } as Cell;
   const filledCell: Cell = { hasTile: () => true } as Cell;
 
+  it('returns empty array when no cells given', () => {
+    const cells: Cell[] = [];
+    expect(generateStartIndices(cells).length).toBe(0);
+  });
+
   it('finds indices where a new word can start', () => {
     const cells = [filledCell, filledCell, emptyCell, emptyCell, emptyCell, filledCell];
     const startIndices = generateStartIndices(cells);
