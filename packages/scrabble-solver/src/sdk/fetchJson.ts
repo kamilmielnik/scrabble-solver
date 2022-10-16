b9a2ee56126137cd38cc5f219f1853111a650e55
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-16 17:08:22 +0700

    Indicate network errors better

--- packages/scrabble-solver/src/sdk/fetchJson.ts
+++ packages/scrabble-solver/src/sdk/fetchJson.ts
@@ -1,13 +1,20 @@
 import { isError } from '@scrabble-solver/types';
 
 const fetchJson = async <T>(input: RequestInfo | URL, init?: RequestInit): Promise<T> => {
-  const response = await fetch(input, {
-    ...init,
-    headers: {
-      'Content-Type': 'application/json',
-      ...init?.headers,
-    },
-  });
+  let response: Response;
+
+  try {
+    response = await fetch(input, {
+      ...init,
+      headers: {
+        'Content-Type': 'application/json',
+        ...init?.headers,
+      },
+    });
+  } catch (error) {
+    const message = isError(error) ? error.message : 'Unknown error';
+    throw new Error(`Network error: ${message}`);
+  }
 
   if (response.ok) {
     return response.json();
