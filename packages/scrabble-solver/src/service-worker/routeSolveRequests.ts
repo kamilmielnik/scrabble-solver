5f778f3485f288c3671e4f48a352f374285abe67
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-22 02:04:12 +0700

    Intercept solve requests and run solve in service worker

--- packages/scrabble-solver/src/service-worker/routeSolveRequests.ts
+++ packages/scrabble-solver/src/service-worker/routeSolveRequests.ts
@@ -1,3 +1,8 @@
+import { Trie } from '@kamilmielnik/trie';
+import { getConfig } from '@scrabble-solver/configs';
+import { BLANK } from '@scrabble-solver/constants';
+import { solve } from '@scrabble-solver/solver';
+import { Board, Locale, Tile } from '@scrabble-solver/types';
 import { registerRoute } from 'workbox-routing';
 
 const headers = {
@@ -7,13 +12,60 @@ const headers = {
 const routeSolveRequests = () => {
   registerRoute(
     ({ url }) => url.pathname === '/api/solve',
-    async () => {
-      const response = {};
-      const json = JSON.stringify(response);
+    async ({ request }) => {
+      const { board, characters, configId, locale } = await request.clone().json();
+      const trie = getTrie(locale);
+
+      if (!trie) {
+        return fetch(request);
+      }
+
+      const config = getConfig(configId)[locale as Locale];
+      const tiles = characters.map((character: string) => new Tile({ character, isBlank: character === BLANK }));
+      const resultsJson = solve(trie, config, Board.fromJson(board), tiles);
+      const json = JSON.stringify(resultsJson);
       return new Response(json, { headers });
     },
     'POST',
   );
+};
+
+const cache: Partial<Record<Locale, Trie | Promise<Trie> | undefined>> = {};
+
+const getTrie = (locale: Locale): Trie | undefined => {
+  const cached = cache[locale];
+
+  if (cached) {
+    if (cached instanceof Promise) {
+      return undefined;
+    }
+
+    return cached;
+  }
+
+  const promise = fetchTrie(locale);
+  cache[locale] = promise;
+  promise
+    .then((trie) => {
+      cache[locale] = trie;
+    })
+    .catch(() => {
+      cache[locale] = undefined;
+    });
+
+  return undefined;
+};
+
+const fetchTrie = async (locale: Locale): Promise<Trie> => {
+  const response = await fetch(`/api/dictionary/${locale}`);
+
+  if (!response.ok) {
+    throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+  }
+
+  const serialized = await response.text();
+  const trie = Trie.deserialize(serialized);
+  return trie;
 };
 
 export default routeSolveRequests;
