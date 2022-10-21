dda15ad16f18cae0eef2c38d1494edeb869ce280
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-22 03:07:30 +0700

    Use Cache API to cache dictionary API responses

--- packages/scrabble-solver/src/service-worker/fetchDictionary.ts
+++ packages/scrabble-solver/src/service-worker/fetchDictionary.ts
@@ -1,16 +1,33 @@
 import { Trie } from '@kamilmielnik/trie';
 import { Locale } from '@scrabble-solver/types';
 
+const DICTIONARY_CACHE = 'dictionary-cache';
+
 const fetchDictionary = async (locale: Locale): Promise<Trie> => {
-  const response = await fetch(`/api/dictionary/${locale}`);
+  const response = await readOrFetchDictionary(`/api/dictionary/${locale}`);
+  const serialized = await response.clone().text();
+  const trie = Trie.deserialize(serialized);
+
+  return trie;
+};
+
+const readOrFetchDictionary = async (url: string): Promise<Response> => {
+  const cache = await caches.open(DICTIONARY_CACHE);
+  const cached = await cache.match(url);
+
+  if (typeof cached !== 'undefined') {
+    return cached;
+  }
+
+  const response = await fetch(url);
 
   if (!response.ok) {
     throw new Error(`HTTP ${response.status}: ${response.statusText}`);
   }
 
-  const serialized = await response.text();
-  const trie = Trie.deserialize(serialized);
-  return trie;
+  cache.put(url, response);
+
+  return response;
 };
 
 export default fetchDictionary;
