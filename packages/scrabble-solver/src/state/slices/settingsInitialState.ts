f1ac318592f50554840accb94261b19d50cbd573
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-23 01:10:41 +0700

    Guess initial locale

--- packages/scrabble-solver/src/state/slices/settingsInitialState.ts
+++ packages/scrabble-solver/src/state/slices/settingsInitialState.ts
@@ -1,10 +1,29 @@
 import { scrabble } from '@scrabble-solver/configs';
 import { Locale } from '@scrabble-solver/types';
 
+const getInitialLocale = (): Locale => {
+  if (!globalThis.navigator) {
+    return Locale.EN_US;
+  }
+
+  const locales = Object.values(Locale);
+  const exactMatch = locales.find((locale) => globalThis.navigator.language === locale);
+
+  if (exactMatch) {
+    return exactMatch;
+  }
+
+  const partialMatch = locales.find((locale) => {
+    return globalThis.navigator.language === locale.substring(0, 2);
+  });
+
+  return partialMatch || Locale.EN_US;
+};
+
 const settingsInitialState = {
   autoGroupTiles: 'left' as 'left' | 'right' | null,
   configId: scrabble.id,
-  locale: Locale.EN_US,
+  locale: getInitialLocale(),
 };
 
 export default settingsInitialState;
