92c1ed6f7f8840d9a2cbeda9f76b6ca8d780ba54
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-04 16:51:44 +0700

    Change German word list to https://github.com/hippler/german-wordlist - Fixes https://github.com/kamilmielnik/scrabble-solver/issues/167

--- packages/word-lists/src/getDeDeWordList.ts
+++ packages/word-lists/src/getDeDeWordList.ts
@@ -1,6 +1,6 @@
 import { getTxtWordList } from './lib';
 
-const FILE_URL = 'https://raw.githubusercontent.com/enz/german-wordlist/master/words';
+const FILE_URL = 'https://raw.githubusercontent.com/hippler/german-wordlist/raw/master/words.txt';
 
 const getDeDeWordList = async (): Promise<string[]> => {
   return getTxtWordList(FILE_URL);
