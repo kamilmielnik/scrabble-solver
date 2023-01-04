70a5eac3c010aa60e934c36ed8f9f77d9ef61659
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-04 20:48:20 +0700

    Fix URL to German dictionary

--- packages/word-lists/src/getDeDeWordList.ts
+++ packages/word-lists/src/getDeDeWordList.ts
@@ -1,6 +1,6 @@
 import { getTxtWordList } from './lib';
 
-const FILE_URL = 'https://raw.githubusercontent.com/hippler/german-wordlist/raw/master/words.txt';
+const FILE_URL = 'https://raw.githubusercontent.com/hippler/german-wordlist/master/words.txt';
 
 const getDeDeWordList = async (): Promise<string[]> => {
   return getTxtWordList(FILE_URL);
