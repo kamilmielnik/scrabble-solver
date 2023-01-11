bb59e7718977608b0987466d827b8e96d5796a30
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-11 21:58:58 +0700

    Fix font flickering

--- packages/scrabble-solver/src/components/SvgFontCss/SvgFontCss.tsx
+++ packages/scrabble-solver/src/components/SvgFontCss/SvgFontCss.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useLayoutEffect } from 'react';
 
 // eslint-disable-next-line max-len
 const CSS = `@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Open+Sans:wght@400;700&family=Roboto+Mono&display=swap');
@@ -7,7 +7,32 @@ text {
   font-family: 'Open Sans';
 }`;
 
-// eslint-disable-next-line react/no-danger
-const SvgFontCss: FunctionComponent = () => <style type="text/css" dangerouslySetInnerHTML={{ __html: CSS }} />;
+let isInitialized = false;
+
+const createSvg = () => {
+  const svg = document.createElement('svg');
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  svg.appendChild(createStyle());
+  return svg;
+};
+
+const createStyle = () => {
+  const style = document.createElement('style');
+  style.innerHTML = CSS;
+  return style;
+};
+
+const SvgFontCss: FunctionComponent = () => {
+  useLayoutEffect(() => {
+    if (!isInitialized) {
+      // prevent text flickering when re-mounting SVGs
+      document.body.appendChild(createSvg());
+      isInitialized = true;
+    }
+  }, []);
+
+  // eslint-disable-next-line react/no-danger
+  return <style type="text/css" dangerouslySetInnerHTML={{ __html: CSS }} />;
+};
 
 export default SvgFontCss;
