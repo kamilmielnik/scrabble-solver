3b9c1a5fa07e65e84a3d09afe1abd00ac1fd5c8a
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-13 21:27:20 +0700

    Use Vazirmatn font for Persian

--- packages/scrabble-solver/src/components/SvgFontCss/SvgFontCss.tsx
+++ packages/scrabble-solver/src/components/SvgFontCss/SvgFontCss.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, useLayoutEffect } from 'react';
 
 // eslint-disable-next-line max-len
-const CSS = `@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Open+Sans:wght@400;700&family=Roboto+Mono&display=swap');
+const CSS = `@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Open+Sans:wght@400;700&family=Vazirmatn:wght@300;400;700&family=Roboto+Mono&display=swap');
 
 text {
   font-family: 'Open Sans';
