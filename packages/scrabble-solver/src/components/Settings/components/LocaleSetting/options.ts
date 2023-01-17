34c1427da3fc11320aabf7b04d3ca1a4ab749a31
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-17 18:06:41 +0700

    Sort languages alphabetically by English name

--- packages/scrabble-solver/src/components/Settings/components/LocaleSetting/options.ts
+++ packages/scrabble-solver/src/components/Settings/components/LocaleSetting/options.ts
@@ -9,6 +9,7 @@ interface Option {
   className: string;
   Icon: FunctionComponent<SVGAttributes<SVGElement>>;
   label: string;
+  name: string;
   value: Locale;
 }
 
@@ -17,44 +18,51 @@ const options: Option[] = [
     className: styles.gb,
     Icon: FlagGb,
     label: 'English (GB)',
+    name: 'English (GB)',
     value: Locale.EN_GB,
   },
   {
     className: styles.us,
     Icon: FlagUs,
     label: 'English (US)',
+    name: 'English (US)',
     value: Locale.EN_US,
   },
   {
     className: styles.fa,
     Icon: FlagFa,
     label: 'فارسی',
+    name: 'Persian',
     value: Locale.FA_IR,
   },
   {
     className: styles.fr,
     Icon: FlagFr,
     label: 'Français',
+    name: 'French',
     value: Locale.FR_FR,
   },
   {
     className: styles.de,
     Icon: FlagDe,
     label: 'Deutsch',
+    name: 'German',
     value: Locale.DE_DE,
   },
   {
     className: styles.pl,
     Icon: FlagPl,
     label: 'Polski',
+    name: 'Polish',
     value: Locale.PL_PL,
   },
   {
     className: styles.es,
     Icon: FlagEs,
     label: 'Español',
+    name: 'Spanish',
     value: Locale.ES_ES,
   },
-];
+].sort((a, b) => a.name.localeCompare(b.name));
 
 export default options;
