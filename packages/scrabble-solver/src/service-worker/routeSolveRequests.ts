64eda1d035cbde9ec77d5573a6187e42d52e8f2a
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-30 00:49:39 +0700

    Do not use client-side solver on slow devices

--- packages/scrabble-solver/src/service-worker/routeSolveRequests.ts
+++ packages/scrabble-solver/src/service-worker/routeSolveRequests.ts
@@ -4,11 +4,25 @@ import { solve } from '@scrabble-solver/solver';
 import { Board, Locale, Tile } from '@scrabble-solver/types';
 import { registerRoute } from 'workbox-routing';
 
+import average from './average';
 import { revalidateDictionary } from './dictionaries';
 import getTrie from './getTrie';
 
 const headers = {
   'Content-Type': 'application/json; charset=utf-8',
+};
+
+const MIN_MEASUREMENTS = 5;
+const localDurations: number[] = [];
+const serverDurations: number[] = [];
+
+const isSlowDevice = (): boolean | undefined => {
+  if (localDurations.length < MIN_MEASUREMENTS || serverDurations.length < MIN_MEASUREMENTS) {
+    return undefined;
+  }
+
+  const count = Math.min(localDurations.length, serverDurations.length);
+  return average(localDurations.slice(0, count)) > average(serverDurations.slice(0, count));
 };
 
 const routeSolveRequests = () => {
@@ -17,18 +31,33 @@ const routeSolveRequests = () => {
     async ({ request }) => {
       const { board, characters, configId, locale } = await request.clone().json();
       const trie = await getTrie(locale);
+      const shouldMeasure = trie && typeof isSlowDevice() === 'undefined';
 
-      if (!trie) {
-        const response = await fetch(request);
-        revalidateDictionary(locale);
-        return response;
+      if (trie && !isSlowDevice()) {
+        const start = Date.now();
+        const config = getConfig(configId)[locale as Locale];
+        const tiles = characters.map((character: string) => new Tile({ character, isBlank: character === BLANK }));
+        const resultsJson = solve(trie, config, Board.fromJson(board), tiles);
+        const json = JSON.stringify(resultsJson);
+
+        if (shouldMeasure) {
+          localDurations.push(Date.now() - start);
+        }
+
+        if (isSlowDevice() === false) {
+          return new Response(json, { headers });
+        }
       }
 
-      const config = getConfig(configId)[locale as Locale];
-      const tiles = characters.map((character: string) => new Tile({ character, isBlank: character === BLANK }));
-      const resultsJson = solve(trie, config, Board.fromJson(board), tiles);
-      const json = JSON.stringify(resultsJson);
-      return new Response(json, { headers });
+      const start = Date.now();
+      const response = await fetch(request);
+
+      if (shouldMeasure) {
+        serverDurations.push(Date.now() - start);
+      }
+
+      revalidateDictionary(locale);
+      return response;
     },
     'POST',
   );
