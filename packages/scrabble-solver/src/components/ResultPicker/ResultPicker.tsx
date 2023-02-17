f7c2485ed48f9bf4adaf2b965ca0193d642a4ea1
Kamil Mielnik <kamil@kamilmielnik.com>
2023-02-17 23:36:43 +0700

    Show real result candidate

--- packages/scrabble-solver/src/components/ResultPicker/ResultPicker.tsx
+++ packages/scrabble-solver/src/components/ResultPicker/ResultPicker.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames';
-import { FunctionComponent, HTMLProps } from 'react';
+import { FunctionComponent, HTMLProps, useEffect } from 'react';
 
 import { ChevronDown } from 'icons';
+import { resultsSlice, selectResultCandidate, selectSortedFilteredResults, useTypedSelector } from 'state';
 
 import styles from './ResultPicker.module.scss';
+import { useDispatch } from 'react-redux';
 
 interface Props extends HTMLProps<HTMLButtonElement> {
   className?: string;
@@ -11,10 +13,24 @@ interface Props extends HTMLProps<HTMLButtonElement> {
 }
 
 const ResultPicker: FunctionComponent<Props> = ({ className, ...props }) => {
+  const dispatch = useDispatch();
+  const resultCandidate = useTypedSelector(selectResultCandidate);
+  const [bestResult] = useTypedSelector(selectSortedFilteredResults)!;
+
+  useEffect(() => {
+    if (bestResult) {
+      dispatch(resultsSlice.actions.changeResultCandidate(bestResult));
+    }
+  }, [bestResult, dispatch]);
+
+  if (!resultCandidate) {
+    return null;
+  }
+
   return (
     <button className={classNames(styles.resultPicker, className)} type="button" {...props}>
-      <div className={styles.points}>328</div>
-      <div className={styles.word}>Essentially</div>
+      <div className={styles.points}>{resultCandidate.points}</div>
+      <div className={styles.word}>{resultCandidate.word}</div>
       <div className={styles.iconContainer}>
         <ChevronDown className={styles.icon} />
       </div>
