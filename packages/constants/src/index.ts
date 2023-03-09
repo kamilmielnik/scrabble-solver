c98a205339764cb23ad27a9de1e1d5a3b0dfeb8d
Kamil Mielnik <kamil@kamilmielnik.com>
2023-03-09 19:22:08 +0700

    Revert change that broke tests

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -8,7 +8,7 @@ export const BONUS_CHARACTER = 'BONUS_CHARACTER';
 
 export const BONUS_WORD = 'BONUS_WORD';
 
-export const EMPTY_CELL = '';
+export const EMPTY_CELL = ' ';
 
 export const NO_BONUS = { characterMultiplier: 1, wordMultiplier: 1 };
 
