0dbc5cf2d46b3de027502a61ff6ff8856db6cff3
Kamil Mielnik <kamil@kamilmielnik.com>
2023-03-12 22:00:16 +0700

    Add explanatory comment

--- packages/scrabble-solver/src/components/Logo/Logo.tsx
+++ packages/scrabble-solver/src/components/Logo/Logo.tsx
@@ -1,3 +1,8 @@
+/**
+ * This component is unused, but it serves as a blueprint for the Logo.svg.
+ * Logo.svg is what this component generates with all the text nodes transformed
+ * into paths (manually with Inkscape), and corner radius removed from tiles.
+ */
 import { CSSProperties, FunctionComponent } from 'react';
 
 import PlainTiles from '../PlainTiles';
