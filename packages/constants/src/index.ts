b1b4d49b58c00926b9b24904a19df88a5afa47ca
Kamil Mielnik <kamil@kamilmielnik.com>
2023-03-16 00:33:31 +0700

    Format code

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -12,34 +12,6 @@ export const EMPTY_CELL = ' ';
 
 export const NO_BONUS = { characterMultiplier: 1, wordMultiplier: 1 };
 
-export const CONSONANTS = [
-  'b',
-  'c',
-  'ć',
-  'd',
-  'f',
-  'g',
-  'h',
-  'j',
-  'k',
-  'l',
-  'ł',
-  'm',
-  'n',
-  'ń',
-  'ñ',
-  'p',
-  'q',
-  'r',
-  's',
-  'ś',
-  't',
-  'v',
-  'w',
-  'x',
-  'z',
-  'ź',
-  'ż',
-];
+export const CONSONANTS = 'bcćdfghjklłmnńñpqrsśtvwxzźż'.split('');
 
-export const VOWELS = ['a', 'ą', 'ä', 'e', 'ę', 'i', 'o', 'ó', 'ö', 'u', 'ü', 'y'];
+export const VOWELS = 'aąäeęioóöuüy'.split('');
