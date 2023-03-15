201a34f55168d75598e9c71ccf26d2de9d2b6cd1
Kamil Mielnik <kamil@kamilmielnik.com>
2023-03-16 02:11:32 +0700

    Use /tmp directory in CI - https://vercel.com/guides/how-can-i-use-files-in-serverless-functions#using-temporary-storage

--- packages/dictionaries/src/constants/index.ts
+++ packages/dictionaries/src/constants/index.ts
@@ -5,6 +5,6 @@ export const DAY = 24 * 60 * 60 * 1000;
 
 export const CACHE_STALE_THRESHOLD = 1 * DAY; // eslint-disable-line no-implicit-coercion
 
-const ROOT_DIRECTORY = process.env.CI ? process.cwd() : os.homedir();
+const ROOT_DIRECTORY = process.env.CI ? '/tmp' : os.homedir();
 
 export const OUTPUT_DIRECTORY = path.resolve(ROOT_DIRECTORY, '.scrabble-solver', 'dictionaries');
