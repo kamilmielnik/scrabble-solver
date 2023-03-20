28014732afcc80de0e2ead0dc7a5ac8d13dd9547
Kamil Mielnik <kamil@kamilmielnik.com>
2023-03-21 00:13:27 +0700

    Instantiate store instantly for global availability

--- packages/scrabble-solver/src/state/index.ts
+++ packages/scrabble-solver/src/state/index.ts
@@ -1,7 +1,7 @@
 export * from './actions';
-export { default as createAppStore } from './createAppStore';
 export { default as localStorage } from './localStorage';
 export * from './selectors';
 export * from './slices';
+export { default as store } from './store';
 export { default as useTranslate } from './useTranslate';
 export { default as useTypedSelector } from './useTypedSelector';
