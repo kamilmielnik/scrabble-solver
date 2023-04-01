306062cf23920ec245cde587744e0625d6b7bd7d
Kamil Mielnik <kamil@kamilmielnik.com>
2023-04-01 18:18:43 +0700

    Make all extractCharactersByCase tests pass

--- packages/scrabble-solver/src/lib/extractCharactersByCase.ts
+++ packages/scrabble-solver/src/lib/extractCharactersByCase.ts
@@ -1,23 +1,28 @@
 import { BLANK } from '@scrabble-solver/constants';
 import { Config } from '@scrabble-solver/types';
 
+import isUpperCase from './isUpperCase';
+
 const extractCharactersByCase = (config: Config, value: string): string[] => {
   let index = 0;
   const characters: string[] = [];
 
   while (index < value.length) {
     const character = value[index];
+    const characterLowercase = value[index].toLocaleLowerCase(config.locale);
     const nextCharacter = value[index + 1];
-    const twoCharacterTileCandidate = `${character}${nextCharacter}`;
+    const digraph = `${character}${nextCharacter}`;
+    const digraphLowercase = digraph.toLocaleLowerCase(config.locale);
 
-    if (config.twoCharacterTiles.includes(twoCharacterTileCandidate)) {
-      characters.push(twoCharacterTileCandidate);
+    if (isUpperCase(config.locale, digraph) && config.twoCharacterTiles.includes(digraphLowercase)) {
+      characters.push(digraphLowercase);
+      index += digraphLowercase.length;
+    } else if (config.hasCharacter(characterLowercase) || characterLowercase === BLANK) {
+      characters.push(characterLowercase);
       ++index;
-    } else if (config.hasCharacter(character) || character === BLANK) {
-      characters.push(character);
+    } else {
+      ++index;
     }
-
-    ++index;
   }
 
   return characters;
