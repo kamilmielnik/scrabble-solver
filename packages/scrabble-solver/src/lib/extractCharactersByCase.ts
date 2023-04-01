82f4dad5a1b41182b0c5eaf20d90ac3a7fc9cb40
Kamil Mielnik <kamil@kamilmielnik.com>
2023-04-01 17:39:52 +0700

    Add extractCharactersByCase with test cases

---
+++ packages/scrabble-solver/src/lib/extractCharactersByCase.ts
@@ -0,0 +1,26 @@
+import { BLANK } from '@scrabble-solver/constants';
+import { Config } from '@scrabble-solver/types';
+
+const extractCharactersByCase = (config: Config, value: string): string[] => {
+  let index = 0;
+  const characters: string[] = [];
+
+  while (index < value.length) {
+    const character = value[index];
+    const nextCharacter = value[index + 1];
+    const twoCharacterTileCandidate = `${character}${nextCharacter}`;
+
+    if (config.twoCharacterTiles.includes(twoCharacterTileCandidate)) {
+      characters.push(twoCharacterTileCandidate);
+      ++index;
+    } else if (config.hasCharacter(character) || character === BLANK) {
+      characters.push(character);
+    }
+
+    ++index;
+  }
+
+  return characters;
+};
+
+export default extractCharactersByCase;
