0f7d0da0f1614c101712218becf8c970b9ed2a0a
Kamil Mielnik <kamil@kamilmielnik.com>
2023-04-19 18:38:11 +0700

    Sort exports

--- packages/configs/src/locales/index.ts
+++ packages/configs/src/locales/index.ts
@@ -2,6 +2,6 @@ export { default as deDe } from './deDe';
 export { default as enGb } from './enGb';
 export { default as enUs } from './enUs';
 export { default as esEs } from './esEs';
-export { default as frFr } from './frFr';
 export { default as faIr } from './faIr';
+export { default as frFr } from './frFr';
 export { default as plPl } from './plPl';
