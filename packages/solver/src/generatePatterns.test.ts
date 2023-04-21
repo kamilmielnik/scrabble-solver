5ad72cfd425e76862889b6446b633f95d39eee61
Kamil Mielnik <kamil@kamilmielnik.com>
2023-04-21 17:19:04 +0700

    Fix vertical results not shown - Fixes https://github.com/kamilmielnik/scrabble-solver/issues/303

---
+++ packages/solver/src/generatePatterns.test.ts
@@ -0,0 +1,13 @@
+import { getConfig } from '@scrabble-solver/configs';
+import { Board, Game, Locale } from '@scrabble-solver/types';
+
+import generatePatterns from './generatePatterns';
+
+describe('generatePatterns', () => {
+  it('Generates all patterns for an empty board', () => {
+    const config = getConfig(Game.Scrabble, Locale.EN_US);
+    const board = Board.create(config.boardWidth, config.boardHeight);
+
+    expect(generatePatterns(config, board).length).toEqual(54);
+  });
+});
