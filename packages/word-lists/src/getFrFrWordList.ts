3c378a181e069658da0449ffa6f96a498e2a22c3
Kamil Mielnik <kamil@kamilmielnik.com>
2023-08-14 03:19:49 +0700

    Use ODS8 French dictionary

--- packages/word-lists/src/getFrFrWordList.ts
+++ packages/word-lists/src/getFrFrWordList.ts
@@ -2,7 +2,7 @@ import { Locale } from '@scrabble-solver/types';
 
 import { getTxtWordList } from './lib';
 
-const FILE_URL = 'https://raw.githubusercontent.com/kamilmielnik/french-dictionary/master/fr-FR.txt';
+const FILE_URL = 'https://raw.githubusercontent.com/Thecoolsim/French-Scrabble-ODS8/main/French%20ODS%20dictionary.txt';
 
 const getFrFrWordList = async (): Promise<string[]> => {
   const words = await getTxtWordList(FILE_URL, Locale.FR_FR);
