b515a78ce49e05d64c92a31d544ff4473fe95edf
Kamil Mielnik <kamil@kamilmielnik.com>
2024-02-11 02:34:31 +0700

    Rename farsi to persian

--- packages/word-definitions/src/crawl.ts
+++ packages/word-definitions/src/crawl.ts
@@ -1,13 +1,13 @@
 import { Locale } from '@scrabble-solver/types';
 
-import { english, farsi, french, german, polish, romanian, spanish } from './languages';
+import { english, french, german, persian, polish, romanian, spanish } from './languages';
 
 const crawlPerLocale: Record<Locale, (word: string) => Promise<string>> = {
   [Locale.DE_DE]: german.crawl,
   [Locale.EN_GB]: english.crawl,
   [Locale.EN_US]: english.crawl,
   [Locale.ES_ES]: spanish.crawl,
-  [Locale.FA_IR]: farsi.crawl,
+  [Locale.FA_IR]: persian.crawl,
   [Locale.FR_FR]: french.crawl,
   [Locale.PL_PL]: polish.crawl,
   [Locale.RO_RO]: romanian.crawl,
--- packages/word-definitions/src/parse.ts
+++ packages/word-definitions/src/parse.ts
@@ -1,6 +1,6 @@
 import { Locale } from '@scrabble-solver/types';
 
-import { english, farsi, french, german, polish, romanian, spanish } from './languages';
+import { english, french, german, persian, polish, romanian, spanish } from './languages';
 import { normalizeDefinition, unique } from './lib';
 import { ParsingResult } from './types';
 
@@ -9,7 +9,7 @@ const parsePerLocale: Record<Locale, (html: string) => ParsingResult> = {
   [Locale.EN_GB]: english.parse,
   [Locale.EN_US]: english.parse,
   [Locale.ES_ES]: spanish.parse,
-  [Locale.FA_IR]: farsi.parse,
+  [Locale.FA_IR]: persian.parse,
   [Locale.FR_FR]: french.parse,
   [Locale.PL_PL]: polish.parse,
   [Locale.RO_RO]: romanian.parse,
