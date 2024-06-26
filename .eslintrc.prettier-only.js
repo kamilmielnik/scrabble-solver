6187dbde2e0907b3c323cac27e6eee393278890d
Kamil Mielnik <kamil@kamilmielnik.com>
2024-06-27 01:19:22 +0700

    Remove .eslintrc.prettier-only.js

--- .eslintrc.prettier-only.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const eslintConfigPath = './.eslintrc.js';
-const baseConfig = require(eslintConfigPath);
-
-module.exports = Object.assign({}, baseConfig, {
-  extends: baseConfig.extends.filter((config) => config.includes('prettier')),
-  rules: {},
-});
