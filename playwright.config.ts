2d54d6c56c6c3be4f81fec18d8f11f04b7f1fcf4
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-13 22:01:34 +0700

    Change default viewport

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,17 +35,26 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1440, height: 900 },
+      },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1440, height: 900 },
+      },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        ...devices['Desktop Safari'],
+        viewport: { width: 1440, height: 900 },
+      },
     },
 
     /* Test against mobile viewports. */
