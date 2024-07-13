cc78f14b408c2f7db1583c96680cda753998f979
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-14 03:30:59 +0700

    Ignore e2e tests in unit tests

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
   transform: {
     '^.+.tsx?$': ['ts-jest', tsConfig],
   },
