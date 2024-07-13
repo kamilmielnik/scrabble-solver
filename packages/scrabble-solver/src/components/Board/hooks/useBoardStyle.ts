bd3bcb7331ce184644518b2863c23971a44a8e58
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-13 22:00:08 +0700

    Get rid of blobs

--- packages/scrabble-solver/src/components/Board/hooks/useBoardStyle.ts
+++ packages/scrabble-solver/src/components/Board/hooks/useBoardStyle.ts
@@ -4,24 +4,20 @@ import { useAppLayout } from 'hooks';
 import { getTileSizes } from 'lib';
 import { selectConfig, selectShowCoordinates, useTypedSelector } from 'state';
 
-import useBackgroundImage from './useBackgroundImage';
-
 const useBoardStyle = () => {
   const config = useTypedSelector(selectConfig);
   const { cellSize } = useAppLayout();
   const { tileFontSize } = getTileSizes(cellSize);
-  const backgroundImage = useBackgroundImage();
   const showCoordinates = useTypedSelector(selectShowCoordinates);
   const boardStyle = useMemo<CSSProperties>(
     () => ({
-      backgroundImage,
       fontSize: tileFontSize,
       gridTemplateColumns:
         showCoordinates === 'hidden' ? `repeat(${config.boardSize}, 1fr)` : `0.5fr repeat(${config.boardSize}, 1fr)`,
       gridTemplateRows:
         showCoordinates === 'hidden' ? `repeat(${config.boardSize}, 1fr)` : `0.5fr repeat(${config.boardSize}, 1fr)`,
     }),
-    [backgroundImage, config.boardSize, tileFontSize],
+    [config.boardSize, tileFontSize],
   );
 
   return boardStyle;
