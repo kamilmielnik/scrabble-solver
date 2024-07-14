7add73cabd1ab63ea10631d4c475b480f63bff21
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-15 02:38:58 +0700

    Fix base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'cypress';
 
-const isProd = process.env.NODE_ENV === 'production';
-const isCi = process.env.CI === 'true';
-
 export default defineConfig({
   e2e: {
-    baseUrl: isProd || isCi ? 'http://localhost:3333' : 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     viewportHeight: 900,
     viewportWidth: 1440,
   },
