861a0ddef2f15c792db9a3fba24c4b9dbdd7d765
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-17 02:34:20 +0700

    Fix board alignment when showing coordinates

--- packages/scrabble-solver/src/components/Board/hooks/useBoardStyle.ts
+++ packages/scrabble-solver/src/components/Board/hooks/useBoardStyle.ts
@@ -2,6 +2,7 @@ import { CSSProperties, useMemo } from 'react';
 
 import { useAppLayout } from 'hooks';
 import { getTileSizes } from 'lib';
+import { BORDER_WIDTH } from 'parameters';
 import { selectConfig, selectShowCoordinates, useTypedSelector } from 'state';
 
 import useBackgroundImage from './useBackgroundImage';
@@ -12,14 +13,19 @@ const useBoardStyle = () => {
   const { tileFontSize } = getTileSizes(cellSize);
   const showCoordinates = useTypedSelector(selectShowCoordinates);
   const backgroundImage = useBackgroundImage();
+  const coordinatesSize = 0.5 * cellSize - BORDER_WIDTH;
   const boardStyle = useMemo<CSSProperties>(
     () => ({
       backgroundImage: `url(${backgroundImage})`,
       fontSize: tileFontSize,
       gridTemplateColumns:
-        showCoordinates === 'hidden' ? `repeat(${config.boardSize}, 1fr)` : `0.5fr repeat(${config.boardSize}, 1fr)`,
+        showCoordinates === 'hidden'
+          ? `repeat(${config.boardSize}, 1fr)`
+          : `${coordinatesSize}px repeat(${config.boardSize}, 1fr)`,
       gridTemplateRows:
-        showCoordinates === 'hidden' ? `repeat(${config.boardSize}, 1fr)` : `0.5fr repeat(${config.boardSize}, 1fr)`,
+        showCoordinates === 'hidden'
+          ? `repeat(${config.boardSize}, 1fr)`
+          : `${coordinatesSize}px repeat(${config.boardSize}, 1fr)`,
     }),
     [backgroundImage, config.boardSize, tileFontSize],
   );
