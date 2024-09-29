915e1f1555ac8dd7fe807ea2978b64919b73f4fd
Kamil Mielnik <kamil@kamilmielnik.com>
2024-09-29 13:15:52 +0700

    Add Romanian Super Scrabble

--- packages/configs/src/languages/romanian.ts
+++ packages/configs/src/languages/romanian.ts
@@ -1,6 +1,6 @@
 import { Config, Locale } from '@scrabble-solver/types';
 
-import { scrabble } from '../games';
+import { scrabble, superScrabble } from '../games';
 
 export const romanianScrabble = new Config({
   ...scrabble,
@@ -31,5 +31,34 @@ export const romanianScrabble = new Config({
     { character: 'v', count: 2, score: 8 },
     { character: 'x', count: 1, score: 10 },
     { character: 'z', count: 1, score: 10 },
+  ],
+});
+
+export const romanianSuperScrabble = new Config({
+  ...superScrabble,
+  locale: Locale.RO_RO,
+  tiles: [
+    { character: 'a', count: 22, score: 1 },
+    { character: 'b', count: 4, score: 9 },
+    { character: 'c', count: 10, score: 1 },
+    { character: 'd', count: 8, score: 2 },
+    { character: 'e', count: 18, score: 1 },
+    { character: 'f', count: 4, score: 8 },
+    { character: 'g', count: 4, score: 9 },
+    { character: 'h', count: 2, score: 10 },
+    { character: 'i', count: 20, score: 1 },
+    { character: 'j', count: 2, score: 10 },
+    { character: 'l', count: 8, score: 1 },
+    { character: 'm', count: 6, score: 4 },
+    { character: 'n', count: 12, score: 1 },
+    { character: 'o', count: 10, score: 1 },
+    { character: 'p', count: 8, score: 2 },
+    { character: 'r', count: 14, score: 1 },
+    { character: 's', count: 10, score: 1 },
+    { character: 't', count: 14, score: 1 },
+    { character: 'u', count: 12, score: 1 },
+    { character: 'v', count: 4, score: 8 },
+    { character: 'x', count: 2, score: 10 },
+    { character: 'z', count: 2, score: 10 },
   ],
 });
