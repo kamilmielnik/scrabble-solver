01f6b88456afd571f5e28d7db9f9a6f8c200b25f
Kamil Mielnik <kamil@kamilmielnik.com>
2025-03-21 03:28:06 +0700

    Update turkish dictionary URL

--- packages/word-lists/src/languages/turkish.ts
+++ packages/word-lists/src/languages/turkish.ts
@@ -2,7 +2,7 @@ import { Locale } from '@scrabble-solver/types';
 
 import { getTxtWordList } from '../lib';
 
-const FILE_URL = 'https://raw.githubusercontent.com/kamilmielnik/scrabble-dictionaries/master/turkish/turkish.txt';
+const FILE_URL = 'https://raw.githubusercontent.com/kamilmielnik/scrabble-dictionaries/master/turkish/kelimelik.txt';
 
 export const getWordList = async (): Promise<string[]> => {
   return getTxtWordList(FILE_URL, Locale.TR_TR);
