0afb1e5d07966257f87469e00749ab3620a3a45e
Kamil Mielnik <kamil@kamilmielnik.com>
2025-12-12 01:30:03 +0700

    Use webpack instead of turbopack because of workbox plugin

--- packages/scrabble-solver/next.config.js
+++ packages/scrabble-solver/next.config.js
@@ -9,21 +9,23 @@ module.exports = {
   sassOptions: {
     loadPaths: ['./src', path.join(__dirname, '../../node_modules/include-media/dist')],
   },
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
-    plugins: [
-      process.env.NODE_ENV === 'production'
-        ? new WorkboxPlugin.InjectManifest({
-            swSrc: path.join(__dirname, 'src/service-worker/index.ts'),
-            swDest: path.join(__dirname, 'public/service-worker.js'),
-            exclude: [/\.map$/, /\.next/, /_next/, /manifest/, /\.htaccess$/, /.*\/static\/.*/, /service-worker\.js$/],
-          })
-        : undefined,
-    ].filter(Boolean),
+  webpack(config, { isServer, dev }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new WorkboxPlugin.InjectManifest({
+          swSrc: path.join(__dirname, 'src/service-worker/index.ts'),
+          swDest: path.join(__dirname, 'public/service-worker.js'),
+          exclude: [/\.map$/, /\.next/, /_next/, /manifest/, /\.htaccess$/, /.*\/static\/.*/, /service-worker\.js$/],
+        }),
+      );
+    }
+
+    return config;
   },
 };
