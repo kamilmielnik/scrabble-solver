7b08ef09f8daeb89b9b5a54a5d30228ca65e2df6
Kamil Mielnik <kamil@kamilmielnik.com>
2026-04-05 18:45:50 +0700

    Upgrade dependencies

--- packages/word-lists/src/lib/transliterateDiacritics.ts
+++ packages/word-lists/src/lib/transliterateDiacritics.ts
@@ -1,7 +1,7 @@
-import { type OptionsTransliterate, transliterate } from 'transliteration';
+import { transliterate } from 'transliteration';
 
 import { unique } from './unique';
 
-export const transliterateDiacritics = (words: string[], options?: OptionsTransliterate): string[] => {
+export const transliterateDiacritics = (words: string[], options?: Parameters<typeof transliterate>[1]): string[] => {
   return unique(words.map((word) => transliterate(word, options)));
 };
