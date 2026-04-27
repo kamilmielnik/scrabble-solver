820a4dc83f4cb5937b92d3980fb012551669c09b
Kamil Mielnik <kamil@kamilmielnik.com>
2026-04-28 04:30:30 +0700

    Format code

--- bun.test.preload.ts
+++ bun.test.preload.ts
@@ -4,8 +4,7 @@ plugin({
   name: 'scss-mock',
   setup(build) {
     build.onLoad({ filter: /\.scss$/ }, () => ({
-      contents:
-        'module.exports = new Proxy({}, { get: (_, key) => key === "__esModule" ? false : key });',
+      contents: 'module.exports = new Proxy({}, { get: (_, key) => key === "__esModule" ? false : key });',
       loader: 'js',
     }));
   },
