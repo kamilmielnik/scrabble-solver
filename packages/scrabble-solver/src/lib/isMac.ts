bc0a0e8921aca6102064a13a5a392466c247e057
Kamil Mielnik <kamil@kamilmielnik.com>
2026-04-28 04:31:57 +0700

    Lint

--- packages/scrabble-solver/src/lib/isMac.ts
+++ packages/scrabble-solver/src/lib/isMac.ts
@@ -3,5 +3,5 @@ export const isMac = (): boolean => {
     return false;
   }
 
-  return globalThis.navigator.platform.startsWith('Mac') || globalThis.navigator.platform === 'iPhone';
+  return globalThis.navigator.platform.startsWith('Mac') || globalThis.navigator.platform.startsWith('iPhone');
 };
